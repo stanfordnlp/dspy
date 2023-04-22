588c7244ce4bc6e0f6f0ae4c5b73ae01de1a8f12
jyx-su <108294040+jyx-su@users.noreply.github.com>
2023-04-22 13:57:00 -0700

    Enable huggingface models

--- dsp/modules/__init__.py
+++ dsp/modules/__init__.py
@@ -1,6 +1,6 @@
 from .cache_utils import *
 from .gpt3 import *
-# from .hf import HFModel
+from .hf import HFModel
 from .colbertv2 import ColBERTv2
 from .sentence_vectorizer import *
 from .cohere import *
