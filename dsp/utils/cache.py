b00d7b1775b881bfe04988a74fc0025a5c4ccb9d
Hanna Moazam <hannamoazam@gmail.com>
2023-06-26 02:15:49 +0300

    Threaded caching support WIP. Tread lightly

--- dsp/utils/cache.py
+++ dsp/utils/cache.py
@@ -1,6 +1,8 @@
 import json
 import time
+import uuid
 import redis
+import random
 import hashlib
 import threading
 from functools import wraps
@@ -13,119 +15,113 @@ MAX_POLL_TIME = 0.200
 
 cache = redis.Redis(host='localhost', port=6379, db=0)
 
+
 def _hash(func, *args, **kwargs):
-    
     func_name = func.__name__ # Joblib code doesn't get func name for lambda functions (see joblib/memory.py:Memory:line 684). TODO - check if should exclude
     id = func_name + str(args) + str(kwargs)
     func_hash = hashlib.sha256(id.encode()).hexdigest()
 
     return func_hash
 
+def _compute(hash, func, *args, **kwargs):
+    worker_id = kwargs.get("worker_id") # required to be there. TODO: see if we can do this without requiring the user to pass in a unique id per thread
+    cache_branch = str(kwargs.get("cache_branch", 0))
 
-def _sort_redis_values(redis_values):
-    
-    # boolean for status gives highest priority to COMPLETE, followed by timestamp ordering
-    # TODO: Better way than sorting each time?
-    sorted_values = sorted(redis_values, key=lambda x: (x["status"] != "COMPLETE", float(x["timestamp"])))
-    return sorted_values
+    key_started = hash + "_" + "branch" + "_" + cache_branch + "STARTED"
+    cache.sadd(key_started, worker_id)
 
+    # remove from kwargs so that don't get passed to the function
+    del kwargs["worker_id"] 
+    if "cache_branch" in kwargs:
+        del kwargs["cache_branch"]
 
-def _find_complete_entry(redis_values):
-    for entry in redis_values:
-        if entry["status"] == "COMPLETE":
-            return entry
-    return None
-
-def _compute(key, func, *args, **kwargs):
-    cache_version = kwargs["cache_version"]
-    # add entry to cache with status = RUNNING
-    ts_str = str(datetime.now(timezone.utc).timestamp())
-    cache.lpush(key, json.dumps({"timestamp": ts_str, "status": "RUNNING", "cache_version": cache_version, "args": args, "kwargs": kwargs}))
-
-    # delete cache_version from kwargs before calling the underlying function
-    del kwargs["cache_version"]
-    # add an entry to the cache with status = RUNNING
     try:
         res = func(*args, **kwargs)
     except Exception as e:
-        # don't cache, so remove RUNNING entry
-        entries = cache.lrange(key, 0, -1)
-        entries = [json.loads(element.decode()) for element in entries]
-        for idx, entry in enumerate(entries):
-            # match the cache version
-            if entry["cache_version"] == cache_version and entry["timestamp"] == ts_str and entry["status"] == "RUNNING":
-                break
-            entries.pop(idx)
-            cache.delete(key)
-            cache.lpush(key, *entries)
-        raise e # TODO raise or return?
+        # don't cache on error, so remove STARTED entry
+        cache.srem(key_started, worker_id)
+        raise e # TODO: check if should raise the error immediately
+        # TODO: see if should also remove the key from redis explicitly (since logic depends on the STARTED key existing)
     else:
-        ts_complete_str = str(datetime.now(timezone.utc).timestamp())
-        # update cache entry with status = COMPLETE, timestamp and result
-        entries = cache.lrange(key, 0, -1)
-        entries = [json.loads(element.decode()) for element in entries]
-        for idx, entry in enumerate(entries):
-            # match the cache version
-            if entry["cache_version"] == cache_version and entry["timestamp"] == ts_str: # TODO: check if this is the right way to match. Can we have multiple entries for the same version with different timestamps?
-                curr = entry
-                break
-        entries.pop(idx)
-        curr["status"] = "COMPLETE"
-        curr["timestamp"] = ts_complete_str
-        curr["result"] = res
+        ts_complete = datetime.now(timezone.utc).timestamp()
+        # check if more recently completed jobs exist in the cache
+        key_complete = hash + "_" + "branch" + "_" + cache_branch + "COMPLETE"
+        if cache.exists(key_complete):
+            id, ts = cache.zrevrange(key_complete, 0, 0, withscores=True)
+            if ts_complete < ts:
+                thread_key = hash + "_" + "branch" + "_" + cache_branch + "_" + "worker" + "_" + id
+                cache.srem(key_started, worker_id) # remove from started
+                result = cache.json().get(thread_key, '.result') 
+                return result
+                # TODO: still need to save the results of this computation? I think not since that would never be the latest? 
+                # But may be needed in advanced ts filtering scenarios
+            
+        # current computation more recent
+        thread_key = hash + "_" + "branch" + "_" + cache_branch + "_" + "worker" + "_" + worker_id
+        cache.srem(key_started, worker_id) # remove from started
+        val = {
+            "status": "COMPLETE", # TODO: I think status is now redundant. 
+            "result": res,
+            "timestamp": ts_complete, # TODO: might not be needed here
+            "fn_name": func.__name__,
+            "args": args,
+            "kwargs": kwargs
+        }
 
-        entries.append(curr)
-        entries = [json.dumps(element).encode() for element in entries]
-        cache.delete(key)
-        cache.lpush(key, *entries)
-        return res
+        cache.json().set(thread_key, '$', val)
+        cache.zadd(key_complete, {worker_id: ts_complete}) 
 
 
 def cache_wrapper(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
-        thread_name = threading.current_thread().name
         # see if cache_version in kwargs
-
-        cache_version = kwargs.get("cache_version", False)
-        if not cache_version:   
-            cache_version = -1 # default cache version. Should document that cache versions start at 0
-            kwargs["cache_version"] = -1
-
-        key = _hash(func, *args, **kwargs)
-
-        if cache.exists(key):
-            # load the list of cached results
-            cached_vals = cache.lrange(key, 0, -1)
-            cached_vals = [json.loads(element.decode()) for element in cached_vals]
-            # sort
-            sorted_vals = _sort_redis_values(cached_vals)
-            # CASE #1 - COMPLETED entry exists in cache - return as is
-            if sorted_vals[0]["status"] == "COMPLETE":
-                print(thread_name + ": CASE 1")
-                return sorted_vals[0]["result"]
-            else:
-                # CASE #2 - Cached entries but COMPLETED entry does not exist in cache - poll
-                print(thread_name + ": CASE 2")
-                start_time = time.time()
-
-                while time.time() - start_time < MAX_POLL_TIME:
-                    # check if an entry has been completed
-                    completed = _find_complete_entry(sorted_vals)
-                    if completed:
-                        return completed["result"]
-                    # Wait for 5 milliseconds
-                    time.sleep(POLL_INTERVAL)
-                
-                # CASE #3A - Cached entries but COMPLETED entry does not yet exist in cache - polling timed out
-                print(thread_name + ":CASE 3A")
-                return _compute(key, func, *args, **kwargs)
+        worker_id = kwargs.get("worker_id") # required to be there. TODO: see if we can do this without requiring the user to pass in a unique id per thread
+        cache_branch = str(kwargs.get("cache_branch", 0))
         
-        else:
-            # CASE #3B - No cached entries - run function and cache
-            print(thread_name + ":CASE 3B")
-            return _compute(key, func, *args, **kwargs)
+        # kwargs["worker_id"] = worker_id
+        # kwargs["cache_branch"] = cache_branch
+
+        hash = _hash(func, *args, **kwargs)
+        key_complete = hash + "_" + "branch" + "_" + cache_branch + "COMPLETE"
+        key_started = hash + "_" + "branch" + "_" + cache_branch + "STARTED"
+
+        # CASE #1 - COMPLETED entry exists in cache - return as is
+        if cache.exists(key_complete):
+            # >= 1 COMPLETE result for this hash + cache_branch exist in the cache
+            # Return result for latest cached result
+
+            id, ts = cache.zrevrange(key_complete, 0, 0, withscores=True) # get the latest
+            print("id: ", id)
+            print("timestamp: ", ts) # TODO: remove withscores after testing
+            thread_key = hash + "_" + "branch" + "_" + cache_branch + "_" + "worker" + "_" + id
+
+            result = cache.json().get(thread_key, '.result')
+            return result
+
+        # CASE #2 - Cached entries but COMPLETED entry does not exist in cache - poll
+        elif cache.exists(key_started):
+            # No COMPLETE result, but >= 1 STARTED jobs
+            start_time = time.time()
+
+            while time.time() - start_time < MAX_POLL_TIME:
+                # TODO: turn the code below into a function as repeat of CASE 1
+                if cache.exists(key_complete):
+                # >= 1 COMPLETE result for this hash + cache_branch exist in the cache
+                # Return result for latest cached result
+
+                    id, ts = cache.zrevrange(key_complete, 0, 0, withscores=True) # get the latest
+                    print("id: ", id)
+                    print("timestamp: ", ts) # TODO: remove withscores after testing
+                    thread_key = hash + "_" + "branch" + "_" + cache_branch + "_" + "worker" + "_" + id
+
+                    result = cache.json().get(thread_key, '.result')
+                    return result
+                time.sleep(POLL_INTERVAL)
         
+        # CASE #3 - No cached entries or polling timed out - run function and cache
+        return _compute(hash, func, *args, **kwargs)
+                                    
     return wrapper
 
 #TODO: Add lru cache
@@ -139,11 +135,10 @@ def add3numbers(a,b,c):
         
 
 def test_function():
-    
-    thread1 = threading.Thread(target=add3numbers, args=(1,2,3), kwargs={"cache_version": 7}, name="Thread 1")
-    thread2 = threading.Thread(target=add3numbers, args=(5,5,7), name="Thread 2")
-    thread3 = threading.Thread(target=add3numbers, args=(5,5,7), name="Thread 3")
-    thread4 = threading.Thread(target=add3numbers, args=(5,5,7), kwargs={"cache_version": 3}, name="Thread 4")
+    thread1 = threading.Thread(target=add3numbers, args=(1,2,3), kwargs={"cache_branch": 7, "worker_id": uuid.uuid4()}, name="Thread 1")
+    thread2 = threading.Thread(target=add3numbers, args=(5,5,7), kwargs={"worker_id": uuid.uuid4()}, name="Thread 2")
+    thread3 = threading.Thread(target=add3numbers, args=(5,5,7), kwargs={"worker_id": uuid.uuid4()}, name="Thread 3")
+    thread4 = threading.Thread(target=add3numbers, args=(5,5,7), kwargs={"cache_branch": 3, "worker_id": uuid.uuid4()}, name="Thread 4")
 
     thread1.start()
     thread2.start()
@@ -166,4 +161,17 @@ if __name__ == "__main__":
     # res3 = add3numbers(61,2,0)
     # print("--4--")
     # res4 = add3numbers(9,1,6,cache_version=3)
-    test_function()
+    # test_function()
+
+    # jane = {
+    #             'name': "Jane",
+    #             'Age': 33,
+    #             'Location': "Chawton"
+    #         }
+
+    # cache.json().set('person:1', '$', jane)
+
+    # result = cache.json().get('person:1')
+    # print(result)
+    # location = cache.json().get('person:1', '.Location')
+    # print(type(location))
