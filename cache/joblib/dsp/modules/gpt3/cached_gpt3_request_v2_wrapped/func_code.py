345109a0d7068608c6eae23e1ef8dc635acac928
Omar Khattab <omar.a.khattab@gmail.com>
2023-08-15 08:44:51 -0700

    Remove DSPv1 caches

--- cache/joblib/dsp/modules/gpt3/cached_gpt3_request_v2_wrapped/func_code.py
+++
@@ -1,5 +0,0 @@
-# first line: 117
-@functools.lru_cache(maxsize=None if cache_turn_on else 0)
-@NotebookCacheMemory.cache
-def cached_gpt3_request_v2_wrapped(**kwargs):
-    return cached_gpt3_request_v2(**kwargs)
