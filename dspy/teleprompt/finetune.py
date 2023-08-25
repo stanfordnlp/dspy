d787d0213b5dbf95781edb3ee35fce2378240bd9
Arnav Singhvi <arnav11.singhvi@gmail.com>
2023-08-25 00:15:31 -0700

    added support for finetuning GPT-3.5

--- dspy/teleprompt/finetune.py
+++ dspy/teleprompt/finetune.py
@@ -46,8 +46,9 @@ fewshot = fewshot_teleprompter.compile(MyMultiHop(passages_per_hop=2), trainset=
 
 
 class BootstrapFinetune(Teleprompter):
-    def __init__(self, metric=None, teacher_settings={}, multitask=True):
+    def __init__(self, metric=None, provider=None, teacher_settings={}, multitask=True):
         self.metric = metric
+        self.provider = provider
         self.teacher_settings = teacher_settings
         self.multitask = multitask
 
@@ -83,8 +84,17 @@ class BootstrapFinetune(Teleprompter):
                 demo = dict(demo)
                 completion = demo.pop(predictor.signature.fields[-1].output_variable)
                 prompt = predictor.signature.query(dsp.Example(demos=[], **demo)).strip()
-
-                finetune_data[name_].append(dict(prompt=prompt, completion=completion))
+                if self.provider == "openai":
+                    example = {
+                        "messages": [
+                            {"role": "system", "content": f"Answer questions with short factoid answers."},
+                            {"role": "user", "content": prompt},
+                            {"role": "assistant", "content": completion}
+                        ]
+                    }
+                elif self.provider == "hf":
+                    example = dict(prompt=prompt, completion=completion)
+                finetune_data[name_].append(example)
 
         for name_ in finetune_data:
             random.Random(0).shuffle(finetune_data[name_])
@@ -127,16 +137,21 @@ class BootstrapFinetune(Teleprompter):
         }
 
         from dsp.modules.finetuning import finetune_hf
+        from dsp.modules.finetuning.finetune_open_ai import finetune_open_ai
 
         target = target
         finetune_models = {}
 
         for name in finetune_data:
             training_data_path = finetune_paths[name]
-            compiler_config_ = dict(compiler_config)
-            compiler_config_['save'] = compiler_config['save'] + '.' + name
-            best_ckpt_path = finetune_hf(training_data_path, target, compiler_config_)
-            finetune_models[name] = dsp.HFModel(model=target, checkpoint=best_ckpt_path) # best_ckpt_path
+            if self.provider == "openai":
+                best_fine_tuned_model = finetune_open_ai(training_data_path, target)
+                finetune_models[name] = dsp.GPT3(model=best_fine_tuned_model)
+            elif self.provider == "hf":
+                compiler_config_ = dict(compiler_config)
+                compiler_config_['save'] = compiler_config['save'] + '.' + name
+                best_ckpt_path = finetune_hf(training_data_path, target, compiler_config_)
+                finetune_models[name] = dsp.HFModel(model=target, checkpoint=best_ckpt_path) # best_ckpt_path
         
         #
         # Set the LMs to the finetuned ones, per module
