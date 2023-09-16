03ed2800c73a7e097cd95cc80afbb5b4b8a92052
Omar Khattab <okhat@users.noreply.github.com>
2023-09-16 09:16:25 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,30 +1,29 @@
 from setuptools import setup, find_packages
 
+# Read the content of the README file
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+# Read the content of the requirements.txt file
+with open('requirements.txt', 'r', encoding='utf-8') as f:
+    requirements = f.read().splitlines()
+
 setup(
     name="dspy-ai",
     version="2.0.3",
     description="DSPy",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url="https://github.com/stanfordnlp/dsp",
     author="Omar Khattab",
     author_email="okhattab@stanford.edu",
     license="MIT License",
     packages=find_packages(include=['dsp.*', 'dspy.*', 'dsp', 'dspy']),
     python_requires='>=3.9',
-    install_requires=[
-        "backoff",
-        "joblib",
-        "jupyter",
-        "openai",
-        "pandas",
-        "spacy",
-        "regex",
-        "ujson",
-        "tqdm",
-        "datasets",        
-    ],
+    install_requires=requirements,
     classifiers=[
-		"Development Status :: 3 - Alpha",
-		"Intended Audience :: Science/Research",
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Science/Research",
         "License :: OSI Approved :: MIT License",
         "Operating System :: POSIX :: Linux",
         "Programming Language :: Python :: 3",
