316f86c267716fefb5af63d11f57c7cac99c1ac4
Sri Vardhamanan <79786680+lawliet19189@users.noreply.github.com>
2023-09-17 23:05:10 +0530

    enhancement(documentation): adding initial docs for dspy LM, RM, teleprompting, local models and generic modules. (#123)

--- dsp/modules/hf_client.py
+++ dsp/modules/hf_client.py
@@ -27,6 +27,7 @@ from dsp.modules.cache_utils import CacheMemory, NotebookCacheMemory, cache_turn
 
 # from dsp.modules.adapter import TurboAdapter, DavinciAdapter, LlamaAdapter
 
+
 class HFClientTGI(HFModel):
     def __init__(self, model, port, url="http://future-hgx-1", **kwargs):
         super().__init__(model=model, is_client=True)
@@ -44,52 +45,58 @@ class HFClientTGI(HFModel):
 
     def _generate(self, prompt, **kwargs):
         kwargs = {**self.kwargs, **kwargs}
-        
+
         payload = {
-        "inputs": prompt,
-        "parameters": {
-            "do_sample": kwargs["n"] > 1,
-            "best_of": kwargs["n"],
-            "details": kwargs["n"] > 1,
-            # "max_new_tokens": kwargs.get('max_tokens', kwargs.get('max_new_tokens', 75)),
-            "stop": ["\n", "\n\n"],
-            **kwargs,
-            }
+            "inputs": prompt,
+            "parameters": {
+                "do_sample": kwargs["n"] > 1,
+                "best_of": kwargs["n"],
+                "details": kwargs["n"] > 1,
+                # "max_new_tokens": kwargs.get('max_tokens', kwargs.get('max_new_tokens', 75)),
+                "stop": ["\n", "\n\n"],
+                **kwargs,
+            },
         }
 
-        payload['parameters'] = openai_to_hf(**payload['parameters'])
+        payload["parameters"] = openai_to_hf(**payload["parameters"])
 
-        payload['parameters']['temperature'] = max(0.1, payload['parameters']['temperature'])
+        payload["parameters"]["temperature"] = max(
+            0.1, payload["parameters"]["temperature"]
+        )
 
         # print(payload['parameters'])
-        
+
         # response = requests.post(self.url + "/generate", json=payload, headers=self.headers)
-        response = send_hftgi_request_v00(self.url + "/generate", json=payload, headers=self.headers)
+        response = send_hftgi_request_v00(
+            self.url + "/generate", json=payload, headers=self.headers
+        )
 
         try:
             json_response = response.json()
             # completions = json_response["generated_text"]
 
-            completions = [json_response['generated_text']]
-            
-            if 'details' in json_response and 'best_of_sequences' in json_response['details']:
-                completions += [x['generated_text'] for x in json_response['details']['best_of_sequences']]
+            completions = [json_response["generated_text"]]
 
-            response = {
-               "prompt": prompt,
-               "choices": [
-                   {"text": c} for c in completions
-               ]
-            }
+            if (
+                "details" in json_response
+                and "best_of_sequences" in json_response["details"]
+            ):
+                completions += [
+                    x["generated_text"]
+                    for x in json_response["details"]["best_of_sequences"]
+                ]
+
+            response = {"prompt": prompt, "choices": [{"text": c} for c in completions]}
             return response
         except Exception as e:
-             print("Failed to parse JSON response:", response.text)
-             raise Exception("Received invalid JSON response from server")
+            print("Failed to parse JSON response:", response.text)
+            raise Exception("Received invalid JSON response from server")
 
 
 @CacheMemory.cache
 def send_hftgi_request_v00(arg, **kwargs):
     return requests.post(arg, **kwargs)
+
 
 class ChatModuleClient(HFModel):
     def __init__(self, model, model_path):
@@ -98,7 +105,9 @@ class ChatModuleClient(HFModel):
         from mlc_chat import ChatModule
         from mlc_chat import ChatConfig
 
-        self.cm = ChatModule(model=model, lib_path=model_path, chat_config=ChatConfig(conv_template="LM"))
+        self.cm = ChatModule(
+            model=model, lib_path=model_path, chat_config=ChatConfig(conv_template="LM")
+        )
 
     def _generate(self, prompt, **kwargs):
         output = self.cm.generate(
@@ -106,11 +115,8 @@ class ChatModuleClient(HFModel):
         )
         try:
             completions = [{"text": output}]
-            response = {
-                "prompt": prompt,
-                "choices": completions
-            }
+            response = {"prompt": prompt, "choices": completions}
             return response
         except Exception as e:
-              print("Failed to parse output:", response.text)
-              raise Exception("Received invalid output")
+            print("Failed to parse output:", response.text)
+            raise Exception("Received invalid output")
