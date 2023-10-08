e9ff35f1461abab32e4231949198ef43a26eff60
Arnav Singhvi <arnav11.singhvi@gmail.com>
2023-10-08 17:22:37 +0000

    revised PoT to match current ReAct module formatting

--- dspy/predict/program_of_thought.py
+++ dspy/predict/program_of_thought.py
@@ -5,19 +5,42 @@ from ..primitives.interpreter import CodePrompt, PythonInterpreter
 import re
 
 class ProgramOfThought(Module):
-    def __init__(self, signature, max_retries=3):
-        previous_code = dspy.InputField(prefix="Previous Code:", desc="previously-generated python code that errored")
-        error = dspy.InputField(prefix="Error:", desc="error message from previously-generated python code")
-        generated_code = dspy.OutputField(prefix="Code:", desc="python code that answers the question") 
-        final_generated_code = dspy.InputField(prefix="Code:", desc="python code that answers the question") 
-        code_output = dspy.InputField(prefix="Code Output:", desc="output of previously-generated python code")
-        code_generate = {"question": signature.kwargs["question"], "code_output": generated_code}
-        code_regenerate = {"question": signature.kwargs["question"],"previous_code": previous_code,"error": error,"generated_code": generated_code}
-        generate_answer = {"question": signature.kwargs["question"],"final_generated_code": final_generated_code,"code_output": code_output,"answer": signature.kwargs["answer"]}
-        self.code_generate = dspy.ChainOfThought(dsp.Template(signature.instructions, **code_generate))
-        self.code_regenerate = dspy.ChainOfThought(dsp.Template(signature.instructions, **code_regenerate))
-        self.generate_answer = dspy.ChainOfThought(dsp.Template(signature.instructions, **generate_answer))
-        self.max_retries = max_retries
+    def __init__(self, signature, max_iters=3):
+        super().__init__()
+
+        self.signature = signature = dspy.Predict(signature).signature
+        self.max_iters = max_iters
+
+        self.input_fields = {k: v for k, v in signature.kwargs.items() if isinstance(v, dspy.InputField)}
+        self.output_fields = {k: v for k, v in signature.kwargs.items() if isinstance(v, dspy.OutputField)}
+
+        inputs, outputs = signature.fields[:-1], signature.fields[-1:]
+
+        inputs_ = ', '.join([f"`{field.input_variable}`" for field in inputs])
+        outputs_ = ', '.join([f"`{field.output_variable}`" for field in outputs])
+
+        assert len(outputs) == 1, "PoT only supports one output field."
+        
+        instr = []
+        instr.append(f"You will be given {', '.join(inputs_)} and you will respond with {', '.join(outputs_)}.\n")
+        instr.append("Answer math questions by generating executable Python code that produces the correct answer.\n")
+
+        self.code_generate = dspy.ChainOfThought(dsp.Template(instr, **self._generate_signature('generate')))
+        self.code_regenerate = dspy.ChainOfThought(dsp.Template(instr, **self._generate_signature('regenerate')))
+        self.generate_answer = dspy.ChainOfThought(dsp.Template(instr, **self._generate_signature('answer')))
+
+    def _generate_signature(self, mode):
+        signature_dict = {}
+        if mode == 'generate':
+            signature_dict['question'] = self.signature.kwargs["question"]
+            signature_dict['code_output'] = dspy.OutputField(prefix="Code:", desc="python code that answers the question")
+        elif mode == 'regenerate':
+            signature_dict.update(self.input_fields)
+            signature_dict['generated_code'] = dspy.OutputField(prefix="Code:", desc="python code that answers the question")
+        else:
+            signature_dict.update(self.input_fields)
+            signature_dict['answer'] = self.signature.kwargs["answer"]
+        return signature_dict
 
     def execute_code(self, code):
         code_match = re.search(r'```python\n(.*?)\n```', code.get('code_output', ''), re.DOTALL)
@@ -32,17 +55,16 @@ class ProgramOfThought(Module):
             return code_match.group(1), None, str(e)
 
     def forward(self, **kwargs):
-        code = self.code_generate(question=kwargs["question"])
-        code, output, error = self.execute_code(code)
-        retries = 0
-        while retries < self.max_retries and error:
+        code_data = self.code_generate(question=kwargs["question"])
+        code, output, error = self.execute_code(code_data)
+        hop = 0
+        while hop < self.max_iters and error:
             print('Error in code execution')
-            code = self.code_regenerate(question=kwargs["question"], previous_code=prev_code, error=error)
-            code, output, error = self.execute_code(code)
-            retries += 1
-            if retries == self.max_retries:
-                print('Max retries reached. Error persists.')
+            code_data = self.code_regenerate(question=kwargs["question"], previous_code=code, error=error)
+            code, output, error = self.execute_code(code_data)
+            hop += 1
+            if hop == self.max_iters:
+                print('Max hops reached. Error persists.')
                 return None
         answer_gen_result = self.generate_answer(question=kwargs["question"], final_generated_code=code, code_output=output)
         return answer_gen_result
-        
