9a67ba99e47f91ca5f44f702ebaf28e5f31c1f5a
Arnav Singhvi <arnav11.singhvi@gmail.com>
2023-11-14 09:36:43 +0000

    updated knn_fewshot with fixes

--- dspy/teleprompt/knn_fewshot.py
+++ dspy/teleprompt/knn_fewshot.py
@@ -3,6 +3,7 @@ import types
 import dsp
 
 from .teleprompt import Teleprompter
+from dspy.teleprompt import BootstrapFewShot
 
 class KNNFewShot(Teleprompter):
     def __init__(self, KNN, k: int, trainset: List[dsp.Example]):
@@ -11,11 +12,11 @@ class KNNFewShot(Teleprompter):
     def compile(self, student, *, teacher=None, trainset, valset=None):
         student_copy = student.reset_copy()
 
-        def forward_pass(**kwargs):
+        def forward_pass(*args, **kwargs):
             knn_trainset = self.KNN(**kwargs)
             few_shot_bootstrap = BootstrapFewShot()
             compiled_program = few_shot_bootstrap.compile(student, teacher=teacher, trainset=knn_trainset, valset=valset)
-            return compiled_program
+            return compiled_program(**kwargs)
         
         student_copy.forward = types.MethodType(forward_pass, student_copy)
         return student_copy
