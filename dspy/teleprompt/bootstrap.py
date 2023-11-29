ca4ce5f8eabb45af69fad7c8db95bdaa637ba2e0
Shangyint <shangyin@berkeley.edu>
2023-11-29 10:25:52 -0800

    set noop handler as default for bootstrap

--- dspy/teleprompt/bootstrap.py
+++ dspy/teleprompt/bootstrap.py
@@ -5,7 +5,7 @@ import threading
 from dspy.predict.retry import Retry
 
 from dspy.primitives import Example
-from dspy.primitives.assertions import assert_no_except_handler, assert_transform_module, bypass_assert_handler, suggest_backtrack_handler
+from dspy.primitives.assertions import assert_transform_module, noop_handler
 
 from .teleprompt import Teleprompter
 from .vanilla import LabeledFewShot
@@ -63,9 +63,8 @@ class BootstrapFewShot(Teleprompter):
 
         assert self.student._compiled is False, "Student must be uncompiled."
 
-        self.student = assert_transform_module(self.student)
-        self.teacher = assert_transform_module(self.teacher, 
-            assertion_handler=lambda fun: suggest_backtrack_handler(assert_no_except_handler(fun)))
+        self.student = assert_transform_module(self.student, noop_handler)
+        self.teacher = assert_transform_module(self.teacher, noop_handler)
 
         if self.max_labeled_demos and self.teacher._compiled is False:
             teleprompter = LabeledFewShot(k=self.max_labeled_demos)
