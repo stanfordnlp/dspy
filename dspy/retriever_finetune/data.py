9ec4cfc8ea2251f718b374605139423758112783
Jasper Xian <xian.jasper@gmail.com>
2023-12-16 21:54:48 +0000

    add probabilistic sampling

--- dspy/retriever_finetune/data.py
+++ dspy/retriever_finetune/data.py
@@ -4,19 +4,27 @@ from tqdm import tqdm
 from dspy.datasets.dataset import Dataset
 from dspy.retriever_finetune import GenerateSearchQuery, TEMPERATURE
 import json
+import numpy as np
+from collections import defaultdict
+import math
     
     
 class RetrievalFinetuneDataset(Dataset):
     def __init__(self, *args, data: Dataset, passages_per_hop=1, max_hops=2, K=20, samples_per_query=10, **kwargs):
         super().__init__(*args, **kwargs)
+        assert samples_per_query < math.comb(K, passages_per_hop)
         self.data = data
         self.generate_query = [dspy.ChainOfThought(GenerateSearchQuery, temperature=TEMPERATURE) for _ in range(max_hops)]
         self.retrieve = dspy.Retrieve(k=K)
         self.max_hops = max_hops
         self.passages_per_hop = passages_per_hop
         self.samples_per_query = samples_per_query
+        self.k = K
         self.query_retrieval_history = {} # query -> topk psgs retrieved
+        self.tuple_history = defaultdict(set) # query -> set of passage tuples we have already seen
         self.id_count = 0
+        self.sample_weights = np.power(np.full(K, 0.8), np.arange(K))
+        self.sample_weights = self.sample_weights / np.sum(self.sample_weights)
         self.generate_dataset()
         
     def retrieve_topK(self, query):
@@ -26,6 +34,15 @@ class RetrievalFinetuneDataset(Dataset):
         topK = self.retrieve(query).passages
         self.query_retrieval_history[query] = topK
         return topK
+    
+    def retrieve_and_sample(self, query):
+        # can be really slow if samples_per_query is just less than comb(K, passages_per_hop), could sample from all perms -> remove seen tuples, but that would be memory expensive
+        topK = self.retrieve_topK(query)
+        while True:
+            passage_indices = tuple(np.random.choice(np.arange(self.k), size=self.passages_per_hop,replace=False, p=self.sample_weights))
+            if passage_indices not in self.tuple_history[query]:
+                self.tuple_history[query].add(passage_indices)
+                return [topK[i] for i in passage_indices]
     
     def generate_dataset(self):
         dataset = []
@@ -37,8 +54,7 @@ class RetrievalFinetuneDataset(Dataset):
                 for hop in range(self.max_hops):
                     query = self.generate_query[hop](context=context, question=question).query
                     queries.append(query)
-                    topK = self.retrieve_topK(query)
-                    passages = random.sample(topK, self.passages_per_hop) # random sample from topK for now, later will use some probabilistic sampling
+                    passages = self.retrieve_and_sample(query)
                     context = context + passages # deduplicate(context + passages)
                 dataset.append({'id': self.id_count, 'question': question, 'context': context, 'answer': row['answer'], 'queries': queries}) # 'supporting_facts': row['supporting_facts']
                 self.id_count += 1
