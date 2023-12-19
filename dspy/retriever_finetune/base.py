37317c5d3a18642a31db521f2cfce620c7c27053
Jasper Xian <xian.jasper@gmail.com>
2023-12-19 23:03:19 +0000

    add fewshotting

--- dspy/retriever_finetune/base.py
+++ dspy/retriever_finetune/base.py
@@ -15,3 +15,14 @@ class GenerateAnswer(dspy.Signature):
     context = dspy.InputField(desc="may contain relevant facts")
     question = dspy.InputField()
     answer = dspy.OutputField(desc="often between 1 and 5 words")
+    
+def validate_context_and_answer_and_hops(example, pred, trace=None):
+    if not dspy.evaluate.answer_exact_match(example, pred): return False
+    if not dspy.evaluate.answer_passage_match(example, pred): return False
+
+    hops = [example.question] + [outputs.query for *_, outputs in trace if 'query' in outputs]
+
+    if max([len(h) for h in hops]) > 100: return False
+    if any(dspy.evaluate.answer_exact_match_str(hops[idx], hops[:idx], frac=0.8) for idx in range(2, len(hops))): return False
+
+    return True
--- dspy/retriever_finetune/generate_tuples.py
+++ dspy/retriever_finetune/generate_tuples.py
@@ -1,7 +1,8 @@
 import dspy
+from dspy.teleprompt import BootstrapFewShot
 from dspy.datasets import HotPotQA
 from dspy.evaluate.evaluate import Evaluate
-from dspy.retriever_finetune import RetrievalFinetuneDataset, RetrievalFinetuneGenerator
+from dspy.retriever_finetune import RetrievalFinetuneDataset, RetrievalFinetuneGenerator, validate_context_and_answer_and_hops
 from collections import defaultdict
 import json
 
@@ -9,22 +10,26 @@ import json
 
 if __name__ == '__main__':
     dataset = HotPotQA(train_seed=1, train_size=20, eval_seed=2023, dev_size=20, test_size=0)
-    trainset = [x.with_inputs('question') for x in dataset.train]
+    hotpot_trainset = [x.with_inputs('question') for x in dataset.train]
     
     colbert = dspy.ColBERTv2(url='http://20.102.90.50:2017/wiki17_abstracts')
     llama2 = dspy.HFModel(model = 'meta-llama/Llama-2-7b-chat-hf')
     dspy.settings.configure(rm=colbert, lm=llama2)
     
-    multihop_dataset = RetrievalFinetuneDataset(data=trainset, K=5, samples_per_query=3)
+    multihop_dataset = RetrievalFinetuneDataset(data=hotpot_trainset, K=5, samples_per_query=3)
     multihop_dataset.save_train('data/dataset-train-colbert-Llama-2-7b-chat-hf.json')
     multihop_dataset.save_dev('data/dataset-dev-colbert-Llama-2-7b-chat-hf.json')
     
+    trainset = [x.with_inputs('question', 'context') for x in multihop_dataset.train]
     evalset = [x.with_inputs('question', 'context') for x in multihop_dataset.dev]
-    uncompiled_generator = RetrievalFinetuneGenerator()
+    # uncompiled_generator = RetrievalFinetuneGenerator()
+
+    teleprompter = BootstrapFewShot(metric=validate_context_and_answer_and_hops)
+    compiled_generator = teleprompter.compile(RetrievalFinetuneGenerator(), teacher=RetrievalFinetuneGenerator(), trainset=trainset)
     
     evaluate_on_hotpotqa = Evaluate(devset=evalset, num_threads=16, display_progress=False, display_table=False)
     metric = dspy.evaluate.answer_exact_match
-    scores = evaluate_on_hotpotqa(uncompiled_generator, metric=metric, return_all_scores=True, display_progress=False, display_table=5)
+    scores = evaluate_on_hotpotqa(compiled_generator, metric=metric, return_all_scores=True, display_progress=False, display_table=5)
     print(scores)
     
     passage_map = defaultdict(dict)
