87d0002fcb7a920cc995534b12fc5b9feebbf0d2
Karel D'Oosterlinck <kareldoosterlinck@gmail.com>
2024-01-25 19:22:04 -0800

    Feature: dspy config and Model singleton

--- dspy/__init__.py
+++ dspy/__init__.py
@@ -27,3 +27,54 @@ HFModel = dsp.HFModel
 
 configure = settings.configure
 context = settings.context
+
+
+import json
+import os
+import openai
+
+class Models:
+    """ Singleton to easily create LMs and keep track of all LMs throughout DSPy."""
+    _instance = None
+    lms = {}
+
+    def __new__(cls, config_path=None):
+        """Singleton constructor. Optionally takes a path to a config which is uses to instantiate LMs."""
+        if not cls._instance:
+            cls._instance = super(Models, cls).__new__(cls)
+            cls._instance._configure_dspy(config_path=config_path)
+        return cls._instance
+
+    @classmethod
+    def create_lm(cls, model_config):
+        """ Create an LM based."""
+        model_config = {**model_config} # deepcopy config
+        model_name = model_config.pop('model')
+
+        if "gpt-" in model_name:
+            lm = OpenAI(model=model_name, **model_config)
+        else:
+            lm = HFClientTGI(
+                model=model_name, port=[7140, 7141, 7142, 7143], **model_config
+            )
+        return lm
+
+    @classmethod
+    def get_lm(cls, config_name):
+        return cls._instance.lms[config_name]
+
+    def _configure_dspy(self, config_path=None):
+        """Read in an LM config, create LMs and save them."""
+        # set openai key
+        openai.api_key = os.environ.get("OPENAI_API_KEY")
+
+        # get LM config
+        self.config_path = config_path
+
+        if self.config_path:
+            with open(self.config_path, "r") as fp:
+                self.config = json.load(fp)
+
+            for config_name, model_config in self.config.items():
+                lm = self.create_lm(model_config)
+                self.lms.update({config_name: lm})
