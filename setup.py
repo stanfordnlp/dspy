6db745a1a2d692105f99e76e19d1a3e95d6d41d8
Omar Khattab <okhat@users.noreply.github.com>
2024-01-31 14:17:24 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('requirements.txt', 'r', encoding='utf-8') as f:
 
 setup(	
     name="dspy-ai",	
-    version="2.1.5",	
+    version="2.1.6",	
     description="DSPy",	
     long_description=long_description,	
     long_description_content_type='text/markdown',	
