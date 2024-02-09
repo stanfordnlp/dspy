45b438ad3c4db251940088e6ea4848c3ef284718
chris levy <christopher@dashhudson.com>
2024-02-09 07:13:37 -0400

    fix(gmsk8_metric): Look for answer in entire LLM output. Not just the first line.

--- dspy/datasets/gsm8k.py
+++ dspy/datasets/gsm8k.py
@@ -1,42 +1,45 @@
-import tqdm
 import random
 
+import tqdm
 from datasets import load_dataset
-from dspy.datasets.dataset import Dataset
+
 
 class GSM8K:
     def __init__(self) -> None:
+        """GSM8K Metric."""
         super().__init__()
         self.do_shuffle = False
 
-        dataset = load_dataset("gsm8k", 'main')
+        dataset = load_dataset("gsm8k", "main")
 
-        hf_official_train = dataset['train']
-        hf_official_test = dataset['test']
+        hf_official_train = dataset["train"]
+        hf_official_test = dataset["test"]
         official_train = []
         official_test = []
 
         for example in tqdm.tqdm(hf_official_train):
-            question = example['question']
+            question = example["question"]
 
-            answer = example['answer'].strip().split()
-            assert answer[-2] == '####'
-            
-            gold_reasoning = ' '.join(answer[:-2])
-            answer = str(int(answer[-1].replace(',', '')))
+            answer = example["answer"].strip().split()
+            if answer[-2] != "####":
+                raise Exception("Error parsing out answer from string.")
 
-            official_train.append(dict(question=question, gold_reasoning=gold_reasoning, answer=answer))
+            gold_reasoning = " ".join(answer[:-2])
+            answer = str(int(answer[-1].replace(",", "")))
+
+            official_train.append({"question": question, "gold_reasoning": gold_reasoning, "answer": answer})
 
         for example in tqdm.tqdm(hf_official_test):
-            question = example['question']
+            question = example["question"]
 
-            answer = example['answer'].strip().split()
-            assert answer[-2] == '####'
-            
-            gold_reasoning = ' '.join(answer[:-2])
-            answer = str(int(answer[-1].replace(',', '')))
+            answer = example["answer"].strip().split()
+            if answer[-2] != "####":
+                raise Exception("Error parsing out answer from string.")
 
-            official_test.append(dict(question=question, gold_reasoning=gold_reasoning, answer=answer))
+            gold_reasoning = " ".join(answer[:-2])
+            answer = str(int(answer[-1].replace(",", "")))
+
+            official_test.append({"question": question, "gold_reasoning": gold_reasoning, "answer": answer})
 
         rng = random.Random(0)
         rng.shuffle(official_train)
@@ -50,9 +53,9 @@ class GSM8K:
 
         import dspy
 
-        trainset = [dspy.Example(**x).with_inputs('question') for x in trainset]
-        devset = [dspy.Example(**x).with_inputs('question') for x in devset]
-        testset = [dspy.Example(**x).with_inputs('question') for x in testset]
+        trainset = [dspy.Example(**x).with_inputs("question") for x in trainset]
+        devset = [dspy.Example(**x).with_inputs("question") for x in devset]
+        testset = [dspy.Example(**x).with_inputs("question") for x in testset]
 
         # print(f"Trainset size: {len(trainset)}")
         # print(f"Devset size: {len(devset)}")
@@ -63,24 +66,22 @@ class GSM8K:
         self.test = testset
 
 
-
-def parse_integer_answer(answer, only_first_line=True):
+def parse_integer_answer(answer) -> int:
+    """Parse integer answer from LLM string output."""
     try:
-        if only_first_line:
-            answer = answer.strip().split('\n')[0]
-
         # find the last token that has a number in it
         answer = [token for token in answer.split() if any(c.isdigit() for c in token)][-1]
-        answer = answer.split('.')[0]
-        answer = ''.join([c for c in answer if c.isdigit()])
+        answer = answer.split(".")[0]
+        answer = "".join([c for c in answer if c.isdigit()])
         answer = int(answer)
 
     except (ValueError, IndexError):
         # print(answer)
         answer = 0
-    
+
     return answer
 
 
-def gsm8k_metric(gold, pred, trace=None):
+def gsm8k_metric(gold, pred) -> bool:
+    """Compare parsed integer answers between gold ground truth and prediction by LLM."""
     return int(parse_integer_answer(str(gold.answer))) == int(parse_integer_answer(str(pred.answer)))
