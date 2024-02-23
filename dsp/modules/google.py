1bfc139bfebf41d4f3f72bdc376367b06442adb5
nbqu <dk41245@gmail.com>
2024-02-23 15:38:07 +0900

    fix API parameters

--- dsp/modules/google.py
+++ dsp/modules/google.py
@@ -1,4 +1,4 @@
-import math
+import os
 from typing import Any, Optional
 import backoff
 
@@ -6,9 +6,12 @@ from dsp.modules.lm import LM
 
 try:
     import google.generativeai as genai
+    from google.api_core.exceptions import GoogleAPICallError
+    google_api_error = GoogleAPICallError
 except ImportError:
     google_api_error = Exception
     print("Not loading Google because it is not installed.")
+
 
 def backoff_hdlr(details):
     """Handler from https://pypi.org/project/backoff/"""
@@ -34,7 +37,7 @@ class Google(LM):
 
     def __init__(
         self,
-        model: str = "gemini-pro-1.0",
+        model: str = "models/gemini-1.0-pro",
         api_key: Optional[str] = None,
         **kwargs
     ):
@@ -51,16 +54,20 @@ class Google(LM):
             Additional arguments to pass to the API provider.
         """
         super().__init__(model)
-        self.google = genai.configure(api_key=self.api_key)
+        api_key = os.environ.get("GOOGLE_API_KEY") if api_key is None else api_key
+        genai.configure(api_key=api_key)
         self.provider = "google"
         self.kwargs = {
-            "model_name": model,
+            "candidate_count": 1,
             "temperature": 0.0 if "temperature" not in kwargs else kwargs["temperature"],
             "max_output_tokens": 2048,
             "top_p": 1,
-            "top_k": 1,
+            "top_k": 40,
             **kwargs
         }
+
+        self.config = genai.GenerationConfig(**self.kwargs)
+        self.llm = genai.GenerativeModel(model_name=model, generation_config=self.config)
 
         self.history: list[dict[str, Any]] = []
 
@@ -68,10 +75,11 @@ class Google(LM):
         raw_kwargs = kwargs
         kwargs = {
             **self.kwargs,
-            "prompt": prompt,
             **kwargs,
         }
-        response = self.co.generate(**kwargs)
+
+        # Google uses "candidate_count" instead of "num_generations"
+        response = self.llm.generate_content(prompt, generation_config=kwargs)
 
         history = {
             "prompt": prompt,
