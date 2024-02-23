ea381fa3c1c20b9ceb7e6ccb21eee694347879c4
KCaverly <kylebcaverly@gmail.com>
2024-02-23 15:09:42 -0500

    feat: added universal caching for all lm calls

--- dspy/backends/lm/base.py
+++ dspy/backends/lm/base.py
@@ -1,16 +1,26 @@
 from abc import ABC, abstractmethod
 
 from pydantic import BaseModel
+from joblib import Memory
+from dsp.modules.cache_utils import cachedir
+
+
+_cache_memory = Memory(cachedir, verbose=0)
 
 
 class BaseLM(BaseModel, ABC):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._cached = _cache_memory.cache(self._call)
+
+    def __call__(self, prompt: str, **kwargs) -> list[str]:
+        """Generates `n` predictions for the signature output."""
+        return self._cached(prompt, **kwargs)
+
     @abstractmethod
-    def __call__(
+    def _call(
         self,
         prompt: str,
-        temperature: float,
-        max_tokens: int,
-        n: int,
         **kwargs,
     ) -> list[str]:
         """Generates `n` predictions for the signature output."""
