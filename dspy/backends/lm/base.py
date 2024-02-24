91d786ebdfbf06d414f675458853ca12e8c38cde
KCaverly <kylebcaverly@gmail.com>
2024-02-23 20:11:13 -0500

    feat: added cache to settings, and make BaseLM caching depend on setting

--- dspy/backends/lm/base.py
+++ dspy/backends/lm/base.py
@@ -1,3 +1,7 @@
+import dspy
+import os
+from pathlib import Path
+import typing as t
 from abc import ABC, abstractmethod
 
 from pydantic import BaseModel
@@ -5,6 +9,7 @@ from joblib import Memory
 from dsp.modules.cache_utils import cachedir
 
 
+cachedir = os.environ.get("DSP_CACHEDIR") or os.path.join(Path.home(), ".joblib_cache")
 _cache_memory = Memory(cachedir, verbose=0)
 
 
@@ -15,14 +20,17 @@ class BaseLM(BaseModel, ABC):
 
     def __call__(self, prompt: str, **kwargs) -> list[str]:
         """Generates `n` predictions for the signature output."""
-        return self._cached(prompt, **kwargs)
+        if dspy.settings.cache:
+            return self._cached(prompt, **kwargs)
+        else:
+            return self._call(prompt, **kwargs)
 
     @abstractmethod
     def _call(
         self,
         prompt: str,
         **kwargs,
-    ) -> list[str]:
+    ) -> list[dict[str, t.Any]]:
         """Generates `n` predictions for the signature output."""
         ...
 
