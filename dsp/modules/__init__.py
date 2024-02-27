c9ad7057725043b9a8b871f90ef428bfa3c4355f
Connor Shorten <connorshorten300@gmail.com>
2024-02-26 21:16:23 -0500

    Update __init__.py
    Rename googlegenerativeai

--- dsp/modules/__init__.py
+++ dsp/modules/__init__.py
@@ -10,7 +10,7 @@ from .pyserini import *
 from .ollama import *
 from .clarifai import *
 from .bedrock import *
-from .google import *
+from .googlegenerativeai import *
 
 
 from .hf_client import HFClientTGI
