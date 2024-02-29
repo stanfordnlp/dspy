a81f9a32a79f96f44c18516e793396f4dd28177a
ragul-kachiappan <rahul.kachiappan@gmail.com>
2024-02-29 14:15:28 +0530

    fix: use generic embedding function as default in ChromadbRM

--- dspy/retrieve/chromadb_rm.py
+++ dspy/retrieve/chromadb_rm.py
@@ -70,11 +70,11 @@ class ChromadbRM(dspy.Retrieve):
         persist_directory: str,
         embedding_function: Optional[
             EmbeddingFunction[Embeddable]
-        ] = None,
+        ] = ef.DefaultEmbeddingFunction(),
         k: int = 7,
     ):
         self._init_chromadb(collection_name, persist_directory)
-        self.ef = embedding_function or self._chromadb_collection._embedding_function
+        self.ef = embedding_function
 
         super().__init__(k=k)
 
