9b3bfca5740ceddd87711e379ba04ea2496660f3
Franck Stephane Ndzomga <ndzomgafs@gmail.com>
2024-03-09 21:06:53 +0100

    (feature) Adding support for Mistral AI's API

--- dsp/modules/__init__.py
+++ dsp/modules/__init__.py
@@ -6,7 +6,11 @@ from .cohere import *
 from .colbertv2 import ColBERTv2
 from .databricks import *
 from .google import *
+from .gpt3 import *
+from .hf import HFModel
+from .hf_client import Anyscale, HFClientTGI, Together
+from .ollama import *
+from .pyserini import *
+from .sbert import *
+from .sentence_vectorizer import *
 from .mistral import *
-from .hf_client import HFClientTGI
-from .hf_client import Anyscale
-from .hf_client import Together
