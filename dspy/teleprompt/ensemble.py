96ee792ed3b5c5a8b5d5e4605863b807f5758eb2
Thomas D Ahle <thomas@ahle.dk>
2024-03-14 21:12:20 -0700

    Formatting

--- dspy/teleprompt/ensemble.py
+++ dspy/teleprompt/ensemble.py
@@ -6,12 +6,13 @@ from dspy.teleprompt.teleprompt import Teleprompter
 TODO: The EnsembledProgram should actually imitate the structure of the individual programs (IF they are all compatible). This allows compiling with an ensemble program as a (singular) teacher. Basically the top majority-compatible trace will end up being used, if dspy.majority is the reduce_fn.
 """
 
+
 class Ensemble(Teleprompter):
     def __init__(self, *, reduce_fn=None, size=None, deterministic=False):
         """A common reduce_fn is dspy.majority."""
-        
+
         assert deterministic is False, "TODO: Implement example hashing for deterministic ensemble."
-        
+
         self.reduce_fn = reduce_fn
         self.size = size
         self.deterministic = deterministic
@@ -21,11 +22,12 @@ class Ensemble(Teleprompter):
         reduce_fn = self.reduce_fn
 
         import dspy
+
         class EnsembledProgram(dspy.Module):
             def __init__(self):
                 super().__init__()
                 self.programs = programs
-            
+
             def forward(self, *args, **kwargs):
                 programs = random.sample(self.programs, size) if size else self.programs
                 outputs = [prog(*args, **kwargs) for prog in programs]
