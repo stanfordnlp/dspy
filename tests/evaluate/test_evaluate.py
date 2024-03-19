29179d245a712668e20c424f886c52cfc182b215
KCaverly <kylebcaverly@gmail.com>
2024-03-19 11:54:08 -0400

    chore: clean up tests/evaluate

--- tests/evaluate/test_evaluate.py
+++ tests/evaluate/test_evaluate.py
@@ -28,33 +28,60 @@ def test_evaluate_initialization():
 
 
 def test_evaluate_call():
+    lm=DummyLM({"What is 1+1?": "2", "What is 2+2?": "4"})
+    with dspy.settings.context(lm=lm, backend=None, cache=False):
+        devset = [new_example("What is 1+1?", "2"), new_example("What is 2+2?", "4")]
+        program = Predict("question -> answer")
+        assert program(question="What is 1+1?").answer == "2"
+        ev = Evaluate(
+            devset=devset,
+            metric=answer_exact_match,
+            display_progress=False,
+        )
+        score = ev(program)
+        assert score == 100.0
+
+def test_evaluate_call_with_backend():
     lm = DummyLanguageModel(answers=[[" 2"], [" 4"]])
     backend = TemplateBackend(lm=lm)
-    dspy.settings.configure(backend=backend, cache=False)
-    devset = [new_example("What is 1+1?", "2"), new_example("What is 2+2?", "4")]
-    program = Predict("question -> answer")
-    ev = Evaluate(
-        devset=devset,
-        metric=answer_exact_match,
-        display_progress=False,
-    )
-    score = ev(program)
-    assert score == 100.0
+    with dspy.settings.context(backend=backend, lm=None, cache=False):
 
+        devset = [new_example("What is 1+1?", "2"), new_example("What is 2+2?", "4")]
+        program = Predict("question -> answer")
+        ev = Evaluate(
+            devset=devset,
+            metric=answer_exact_match,
+            display_progress=False,
+        )
+        score = ev(program)
+        assert score == 100.0
 
 def test_evaluate_call_bad():
+    lm=DummyLM({"What is 1+1?": "0", "What is 2+2?": "0"})
+    with dspy.settings.context(backend=None, lm=lm):
+        devset = [new_example("What is 1+1?", "2"), new_example("What is 2+2?", "4")]
+        program = Predict("question -> answer")
+        ev = Evaluate(
+            devset=devset,
+            metric=answer_exact_match,
+            display_progress=False,
+        )
+        score = ev(program)
+        assert score == 0.0
+
+def test_evaluate_call_bad_with_backend():
     lm = DummyLanguageModel(answers=[[" 0"], [" 0"]])
     backend = TemplateBackend(lm=lm)
-    dspy.settings.configure(backend=backend, cache=False)
-    devset = [new_example("What is 1+1?", "2"), new_example("What is 2+2?", "4")]
-    program = Predict("question -> answer")
-    ev = Evaluate(
-        devset=devset,
-        metric=answer_exact_match,
-        display_progress=False,
-    )
-    score = ev(program)
-    assert score == 0.0
+    with dspy.settings.context(backend=backend, lm=None, cache=False):
+        devset = [new_example("What is 1+1?", "2"), new_example("What is 2+2?", "4")]
+        program = Predict("question -> answer")
+        ev = Evaluate(
+            devset=devset,
+            metric=answer_exact_match,
+            display_progress=False,
+        )
+        score = ev(program)
+        assert score == 0.0
 
 
 def test_evaluate_display_table():
