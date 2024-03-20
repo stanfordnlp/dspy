d0271f87bb5b07fa1b2fc645a583dd62857cc556
KCaverly <kylebcaverly@gmail.com>
2024-03-20 11:22:17 -0400

    chore: ruff fixes for logging

--- dspy/utils/logging.py
+++ dspy/utils/logging.py
@@ -1,13 +1,13 @@
 
-import structlog
 import logging
 import os
 
+import structlog
+
 level = os.environ.get("LOG_LEVEL", "INFO").upper()
 
-def set_log_level(level: str):
-    """Set the logging level"""
-
+def set_log_level(level: str) -> None:
+    """Set the logging level."""
     level = level.upper()
     if level not in ["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"]:
         raise ValueError("log level provider ({level}) is not one of DEBUG, INFO, WARNING, ERROR, CRITICAL")
