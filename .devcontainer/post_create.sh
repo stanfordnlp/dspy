227fd8a8ed002b0bf67bfb31e4380f0d8d62b5ec
Josh <josh.mantova@gmail.com>
2024-03-24 01:29:26 +0000

    enh(devcontainer): add pre-commit hooks

--- .devcontainer/post_create.sh
+++ .devcontainer/post_create.sh
@@ -2,11 +2,14 @@
 
 git config --global --add safe.directory /workspaces/dspy
 
-pip install poetry
+pip install poetry==1.7.1
 
-poetry install --all-extras
+poetry install --with dev
 
 sudo apt update
+sudo apt-get -y install python3-distutils
+
+poetry run pre-commit install --install-hooks
 
 personalization_script="./.devcontainer/.personalization.sh"
 
