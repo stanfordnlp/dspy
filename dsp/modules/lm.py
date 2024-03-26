0e91c6b5a6b5c1b3440a0c50f377fe568d80bc85
Connor Shorten <connorshorten300@gmail.com>
2024-03-25 20:28:57 -0400

    Fix for history, only returning the `text` from Cohere LM

--- dsp/modules/lm.py
+++ dsp/modules/lm.py
@@ -52,7 +52,7 @@ class LM(ABC):
                     blocks = [{"text": block.text} for block in x["response"].content if block.type == "text"]
                     printed.append((prompt, blocks))
                 elif provider == "cohere":
-                    printed.append((prompt, x["response"].generations))
+                    printed.append((prompt, x["response"]))
                 elif provider == "mistral":
                     printed.append((prompt, x['response'].choices))
                 else:
