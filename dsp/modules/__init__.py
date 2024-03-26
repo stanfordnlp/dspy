3400fc3ccc267ee785c845c3ce27141cff08ed8a
Somesh <42097653+someshfengde@users.noreply.github.com>
2024-03-26 22:29:44 +0530

    Update __init__.py

--- dsp/modules/__init__.py
+++ dsp/modules/__init__.py
@@ -9,7 +9,6 @@ from .databricks import *
 from .google import *
 from .gpt3 import *
 from .groq_client import *
-from .together_client import *
 from .hf import HFModel
 from .hf_client import Anyscale, HFClientTGI, Together
 from .mistral import *
