109482ef5358a120857988d9dbccca844661f347
Harry Salmon <harry@harrysalmon.co.uk>
2024-03-28 18:58:04 +0100

    Add failing test: test_openai_vectorizer
    >           cur_batch_embeddings = [cur_obj['embedding'] for cur_obj in response['data']]
    E           TypeError: 'CreateEmbeddingResponse' object is not subscriptable

---
+++ tests/retrieve/sentence_vectorizer/test_openai_vectorizer.py
@@ -0,0 +1,27 @@
+import os
+import pytest
+
+from dspy import Example
+from dsp import OpenAIVectorizer
+
+
+@pytest.mark.skipif(os.getenv('OPENAI_API_KEY') is None, reason="Skipping this test because OPENAI_API_KEY is not set")
+def test__call__():
+    # Currently these classes are used to vectorise the input sections of an Example class.
+    vectorizer = OpenAIVectorizer()
+    input_examples = [
+        example.with_inputs('question')
+        for example in [
+            Example(question="Why?", answer="Who knows?"),
+            Example(question="Why not?", answer="I don't know?"),
+        ]
+    ]
+    vectorizer(inp_examples=input_examples)
+
+
+@pytest.mark.skipif(os.getenv('OPENAI_API_KEY') is None, reason="Skipping this test because OPENAI_API_KEY is not set")
+def test__call__with_str():
+    # Currently these classes are used to vectorise the input sections of an Example class.
+    vectorizer = OpenAIVectorizer()
+    vectorizer("Hello world")
+
