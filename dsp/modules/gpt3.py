21d84b4d012ec4b40bce5d9025201c3aa4c875a9
xdite <9004+xdite@users.noreply.github.com>
2024-04-03 10:43:46 +0800

    remove top_p default

--- dsp/modules/gpt3.py
+++ dsp/modules/gpt3.py
@@ -87,7 +87,6 @@ class GPT3(LM):
         self.kwargs = {
             "temperature": 0.0,
             "max_tokens": 150,
-            "top_p": 1,
             "frequency_penalty": 0,
             "presence_penalty": 0,
             "n": 1,
