e0b8a691d607ca7b4a5ad46805b9761e9f975cb3
Robert P. Goldman <rpgoldman@sift.net>
2024-05-05 11:17:02 -0500

    Remove unnecessary return value.

--- dspy/teleprompt/teleprompt_optuna.py
+++ dspy/teleprompt/teleprompt_optuna.py
@@ -52,9 +52,7 @@ class BootstrapFewShotWithOptuna(Teleprompter):
             display_table=False,
             display_progress=True,
         )
-        # FIXME: this should have return_all_scores=False and then need only score,
-        # since only one value will be returned. - rpg
-        score, _ = evaluate(program2, return_all_scores=True)
+        score = evaluate(program2, return_all_scores=False)
         trial.set_user_attr("program", program2)
         return score
 
