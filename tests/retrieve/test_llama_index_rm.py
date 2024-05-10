269d9e8822ce73839a85f896db7ca6bcccbb2494
Patrick Miramontes <patdjm7@gmail.com>
2024-05-10 00:49:22 +0000

    fix(dependencies): updated LI tests to be optional

--- tests/retrieve/test_llama_index_rm.py
+++ tests/retrieve/test_llama_index_rm.py
@@ -1,18 +1,27 @@
+import logging
+
 import pytest
-from llama_index.core import Settings, VectorStoreIndex
-from llama_index.core.base.base_retriever import BaseRetriever
-from llama_index.core.embeddings.mock_embed_model import MockEmbedding
-from llama_index.core.readers.string_iterable import StringIterableReader
 
 import dspy
 from dsp.modules.dummy_lm import DummyLM
 from dspy.datasets import HotPotQA
-from dspy.retrieve.llama_index_rm import LlamaIndexRM
+
+try:
+    from llama_index.core import Settings, VectorStoreIndex
+    from llama_index.core.base.base_retriever import BaseRetriever
+    from llama_index.core.embeddings.mock_embed_model import MockEmbedding
+    from llama_index.core.readers.string_iterable import StringIterableReader
+
+    from dspy.retrieve.llama_index_rm import LlamaIndexRM
+
+except ImportError:
+    logging.info("Optional dependency llama-index is not installed - skipping LlamaIndexRM tests.")
 
 
 @pytest.fixture()
 def rag_setup() -> dict:
     """Builds the necessary fixtures to test LI"""
+    pytest.importorskip("llamaindex")
     dataset = HotPotQA(train_seed=1, train_size=8, eval_seed=2023, dev_size=4, test_size=0)
     trainset = [x.with_inputs("question") for x in dataset.train]
     devset = [x.with_inputs("question") for x in dataset.dev]
@@ -37,7 +46,7 @@ def rag_setup() -> dict:
 
 def test_lirm_as_rm(rag_setup):
     """Test the retriever as retriever method"""
-
+    pytest.importorskip("llamaindex")
     retriever = rag_setup.get("retriever")
     test_res_li = retriever.retrieve("At My Window was released by which American singer-songwriter?")
     rm = rag_setup.get("rm")
