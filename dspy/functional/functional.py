7e875950b99ce93e8d0dbfc4231a583898ed252f
JONEMI19 <michael.jones4@kingfisher.com>
2024-05-22 08:20:17 +0000

    feat(dspy): update mechanism by which paser is applied

--- dspy/functional/functional.py
+++ dspy/functional/functional.py
@@ -1,6 +1,7 @@
 import inspect
 import json
 import typing
+from contextlib import suppress
 from typing import Annotated, Callable, List, Tuple, Union  # noqa: UP035
 
 import pydantic
@@ -405,17 +406,16 @@ def _func_to_signature(func):
     return dspy.Signature(fields, instructions)
 
 
-def _unwrap_json(output, native_parser: Callable[[str], Union[dict, pydantic.BaseModel]]):
-    try:
-        return native_parser(output)
-    except (ValueError, pydantic.ValidationError):
-        output = output.strip()
-        if output.startswith("```"):
-            if not output.startswith("```json"):
-                raise ValueError("json output should start with ```json") from None
-            if not output.endswith("```"):
-                raise ValueError("Don't write anything after the final json ```") from None
-            output = output[7:-3].strip()
-        if not output.startswith("{") or not output.endswith("}"):
-            raise ValueError("json output should start and end with { and }") from None
-        return ujson.dumps(ujson.loads(output))  # ujson is a bit more robust than the standard json
+def _unwrap_json(output, from_json: Callable[[str], Union[pydantic.BaseModel, str]]):
+    with suppress(ValueError, pydantic.ValidationError, AttributeError):
+        output = from_json(output).model_dump_json()
+    output = output.strip()
+    if output.startswith("```"):
+        if not output.startswith("```json"):
+            raise ValueError("json output should start with ```json") from None
+        if not output.endswith("```"):
+            raise ValueError("Don't write anything after the final json ```") from None
+        output = output[7:-3].strip()
+    if not output.startswith("{") or not output.endswith("}"):
+        raise ValueError("json output should start and end with { and }") from None
+    return ujson.dumps(ujson.loads(output))  # ujson is a bit more robust than the standard json
