79896f57e0c280d0fd8df416e00924d431d5e54e
Adam <adam@getsimple.rsvp>
2024-06-13 10:23:52 -0400

    add few shot bootstrap args

--- dspy/teleprompt/knn_fewshot.py
+++ dspy/teleprompt/knn_fewshot.py
@@ -9,15 +9,16 @@ from .teleprompt import Teleprompter
 
 
 class KNNFewShot(Teleprompter):
-    def __init__(self, k: int, trainset: List[dsp.Example], vectorizer: Optional[dsp.BaseSentenceVectorizer] = None):
+    def __init__(self, k: int, trainset: List[dsp.Example], vectorizer: Optional[dsp.BaseSentenceVectorizer] = None, **few_shot_bootstrap_args):
         self.KNN = KNN(k, trainset, vectorizer=vectorizer)
+        self.few_shot_bootstrap_args = few_shot_bootstrap_args
 
     def compile(self, student, *, teacher=None, trainset=None, valset=None):
         student_copy = student.reset_copy()
 
         def forward_pass(_, *args, **kwargs):
             knn_trainset = self.KNN(**kwargs)
-            few_shot_bootstrap = BootstrapFewShot()
+            few_shot_bootstrap = BootstrapFewShot(**self.few_shot_bootstrap_args)
             compiled_program = few_shot_bootstrap.compile(
                 student,
                 teacher=teacher,
