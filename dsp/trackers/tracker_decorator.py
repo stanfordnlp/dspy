b23818bb60f84bc3bc1e9be72fba336d133f033b
xucai <l1240626535@gmail.com>
2024-07-05 00:14:55 +0800

    support tracker_decorator

--- dsp/trackers/tracker_decorator.py
+++ dsp/trackers/tracker_decorator.py
@@ -2,7 +2,8 @@
 def tracker_decorator(func):
     def tracker_wrapper(*args, **kwargs):
         completions = func(*args, **kwargs)
-        if hasattr(args[0], "tracker"):
-            args[0].tracker.call(i=kwargs['prompt'], o=completions, **kwargs)
+        from dsp import BaseTracker
+        if hasattr(args[0], "tracker") and issubclass(args[0], BaseTracker):
+            args[0].tracker.call(i=args[1], o=completions, **kwargs)
         return completions
     return tracker_wrapper
