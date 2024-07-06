415651bd7cc56de7f3595ab5baaf31756cc886d6
Omar Khattab <okhattab@stanford.edu>
2024-07-06 14:20:33 -0700

    Fix circular import

--- dsp/adapters/__init__.py
+++ dsp/adapters/__init__.py
@@ -1,5 +1,4 @@
-from .base_template import *
-from .experimental_adapter import *
-from .template import *
+from .base_template import BaseTemplate
+from .experimental_adapter import ExperimentalAdapter
+from .template import Template
 from .utils import *
-
