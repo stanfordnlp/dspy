3df9fba969d8315e121d9a3c258099a4ba157476
Oscar Arroyo Vega <arroscar@gmail.com>
2024-07-17 01:00:24 +0000

    fix(dsp/modules/unif.py): solved pass endpoint also if model and provider are given

--- dsp/modules/unify.py
+++ dsp/modules/unify.py
@@ -17,66 +17,62 @@ class Unify(LM):
         api_key: Optional[str] = None,
         system_prompt: Optional[str] = None,
         n: int = 1,
-        base_url: str = "https://api.unify.ai/v0",
         **kwargs,
     ):
         self.api_key = api_key
-        self.model = model
-        self.model_provider = model_provider
-        self.endpoint = endpoint
-        self.stream = stream
+        if endpoint:
+            self.endpoint = endpoint
+        elif model and model_provider:
+            self.endpoint = "@".join([model, model_provider])
+
         self.client = UnifyClient(
-            api_key=self.api_key, endpoint=self.endpoint, model=self.model, provider=self.model_provider
+            api_key=self.api_key,
+            endpoint=self.endpoint,
         )
+
         super().__init__(model=self.endpoint)
 
         self.provider = "unify"
+        self.stream = stream
         self.system_prompt = system_prompt
         self.kwargs = {
+            "model": self.endpoint,
             "temperature": 0.0,
             "max_tokens": 150,
             "top_p": 1,
-            "frequency_penalty": 0,
-            "presence_penalty": 0,
             "n": n,
             **kwargs,
         }
-        self.kwargs["endpoint"] = self.endpoint
+
         self.history: list[dict[str, Any]] = []
 
-    def basic_request(self, prompt: str, **kwargs) -> Any:
+    def basic_request(
+        self,
+        prompt: str,
+        **kwargs,
+    ) -> Any:
         """Basic request to the Unify's API."""
-        raw_kwargs = kwargs
         kwargs = {**self.kwargs, **kwargs}
         messages = [{"role": "user", "content": prompt}]
 
-        settings_dict = {
-            "endpoint": self.endpoint,
-            "stream": self.stream,
-            "messages": messages,
-        }
         if self.system_prompt:
-            settings_dict["messages"].insert(0, {"role": "system", "content": self.system_prompt})
+            messages.insert(0, {"role": "system", "content": self.system_prompt})
 
         raw_response = self.client.generate(
-            messages=settings_dict["messages"],
-            stream=settings_dict["stream"],
+            messages=messages,
+            stream=self.stream,
             temperature=kwargs["temperature"],
             max_tokens=kwargs["max_tokens"],
         )
 
         # condition to handle both outputs stream =True or False from Unify generate method
-        if isinstance(raw_response, str):
-            response_content = raw_response
-        else:
-            response_content = "".join(chunk for chunk in raw_response)
+        response_content = raw_response if isinstance(raw_response, str) else "".join(raw_response)
         formated_response: dict = {"choices": [{"message": {"content": response_content}}]}  # response with choices
 
         history = {
             "prompt": prompt,
             "response": formated_response,
             "kwargs": kwargs,
-            "raw_kwargs": raw_kwargs,
         }
 
         self.history.append(history)
