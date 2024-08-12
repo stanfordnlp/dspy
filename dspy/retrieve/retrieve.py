3b0911557760c5b9c551a76b1c9564d312229881
Connor Shorten <connorshorten300@gmail.com>
2024-08-12 16:05:05 -0400

    Update retrieve.py
    Adds save_field_meta default.

--- dspy/retrieve/retrieve.py
+++ dspy/retrieve/retrieve.py
@@ -28,7 +28,8 @@ class Retrieve(Parameter):
     def reset(self):
         pass
 
-    def dump_state(self):
+    def dump_state(self, save_field_meta=False):
+        """save_field_meta is set as a default argument to support the Parameter interface for dump_state()"""
         state_keys = ["k"]
         return {k: getattr(self, k) for k in state_keys}
 
