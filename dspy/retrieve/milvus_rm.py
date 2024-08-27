483672004d24d69d6e3dc0eba7a9479ee2af00dc
Arya Krishna <Arya.Krishna@ril.com>
2024-08-27 19:58:32 +0530

    Changed top_k to k in milvus rm due to error : AttributeError: 'MilvusRM' object has no attribute 'k'

--- dspy/retrieve/milvus_rm.py
+++ dspy/retrieve/milvus_rm.py
@@ -82,7 +82,7 @@ class MilvusRM(dspy.Retrieve):
         self.collection_name = collection_name
 
         self.embedding_function = embedding_function or openai_embedding_function
-        self.top_k = k
+        self.k = k
 
     def forward(self, query_or_queries: Union[str, List[str]], k: Optional[int] = None) -> dspy.Prediction:
         if isinstance(query_or_queries, str):
