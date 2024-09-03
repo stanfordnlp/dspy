0c34abd3aada22a95af1c2bd0e35e16e4e942ae7
alejandro <alejandro.herrera@snowflake.com>
2024-09-03 19:24:49 +0000

    refactor(dspy): updating snowflake LM and RM implementations

--- dspy/retrieve/snowflake_rm.py
+++ dspy/retrieve/snowflake_rm.py
@@ -1,14 +1,11 @@
+import json
 from typing import Optional, Union
 
 import dspy
 from dsp.utils import dotdict
 
 try:
-    from snowflake.snowpark import Session
-    from snowflake.snowpark import functions as snow_fn
-    from snowflake.snowpark.functions import col, function, lit
-    from snowflake.snowpark.types import VectorType
-
+    from snowflake.core import Root
 except ImportError:
     raise ImportError(
         "The snowflake-snowpark-python library is required to use SnowflakeRM. Install it with dspy-ai[snowflake]",
@@ -16,41 +13,47 @@ except ImportError:
 
 
 class SnowflakeRM(dspy.Retrieve):
-    """A retrieval module that uses Weaviate to return the top passages for a given query.
+    """A retrieval module that uses Snowlfake's Cortex Search service to return the top relevant passages for a given query.
 
-    Assumes that a Snowflake table has been created and populated with the following payload:
-        - content: The text of the passage
+    Assumes that a Snowflake Cortex Search endpoint has been configured by the use.
+
+    For more information on configuring the Cortex Search service, visit: https://docs.snowflake.com/en/user-guide/snowflake-cortex/cortex-search/cortex-search-overview
 
     Args:
-        snowflake_credentials: connection parameters for initializing Snowflake client.
-        snowflake_table_name (str): The name of the Snowflake table containing document embeddings.
-        embeddings_field (str): The field in the Snowflake table with the content embeddings
-        embeddings_text_field (str): The field in the Snowflake table with the content.
+        snowflake_sesssion (object): Snowflake Snowpark session for accessing the service.
+        cortex_search_service(str): Name of the Cortex Search service to be used.
+        snowflake_database (str): The name of the Snowflake table containing document embeddings.
+        snowflake_schema (str): The name of the Snowflake table containing document embeddings.
         k (int, optional): The default number of top passages to retrieve. Defaults to 3.
     """
 
     def __init__(
         self,
-        snowflake_table_name: str,
-        snowflake_credentials: dict,
+        snowflake_session: dict,
+        cortex_search_service: str,
+        snowflake_database: str,
+        snowflake_schema: str,
         k: int = 3,
-        embeddings_field: str = "chunk_vec",
-        embeddings_text_field: str = "chunk",
-        embeddings_model: str = "e5-base-v2",
     ):
-        self.snowflake_table_name = snowflake_table_name
-        self.embeddings_field = embeddings_field
-        self.embeddings_text_field = embeddings_text_field
-        self.embeddings_model = embeddings_model
-        self.client = self._init_cortex(credentials=snowflake_credentials)
-
         super().__init__(k=k)
+        self.k = k
+        self.cortex_search_service_name = cortex_search_service
+        self.client = self._fetch_cortex_service(
+            snowflake_session, snowflake_database, snowflake_schema, cortex_search_service
+        )
 
-    def forward(self, query_or_queries: Union[str, list[str]], k: Optional[int] = None) -> dspy.Prediction:
-        """Search Snowflake document embeddings table for self.k top passages for query.
-
+    def forward(
+        self,
+        query_or_queries: Union[str, list[str]],
+        response_columns: list[str],
+        filters: dict = None,
+        k: Optional[int] = None,
+    ) -> dspy.Prediction:
+        """Query Cortex Search endpoint for top relevant passages.
         Args:
             query_or_queries (Union[str, List[str]]): The query or queries to search for.
+            repsonse_columns (List[str]): A comma-separated list of columns to return for each relevant result in the response. These columns must be included in the source query for the service.
+            filters (dict): A filter object for filtering results based on data in the ATTRIBUTES columns. See Filter syntax.
             k (Optional[int]): The number of top passages to retrieve. Defaults to self.k.
 
         Returns:
@@ -61,56 +64,48 @@ class SnowflakeRM(dspy.Retrieve):
         queries = [q for q in queries if q]
         passages = []
 
-        for query in queries:
-            query_embeddings = self._get_embeddings(query)
-            top_k_chunks = self._top_k_similar_chunks(query_embeddings, k)
+        for cortex_query in queries:
+            response_chunks = self._query_cortex_search(
+                cortex_search_service=self.client, query=cortex_query, columns=response_columns, filter=filters, k=k
+            )
 
-            passages.extend(dotdict({"long_text": passage[0]}) for passage in top_k_chunks)
+            if len(response_columns) == 1:
+                passages.extend(
+                    dotdict({"long_text": passage[response_columns[0]]}) for passage in response_chunks["results"]
+                )
+            else:
+                passages.extend(dotdict({"long_text": str(passage)}) for passage in response_chunks["results"])
 
         return passages
 
-    def _top_k_similar_chunks(self, query_embeddings, k):
-        """Search Snowflake table for self.k top passages for query.
+    def _fetch_cortex_service(self, snowpark_session, snowflake_database, snowflake_schema, cortex_search_service_name):
+        """Fetch the Cortex Search service to be used"""
+        snowpark_session.query_tag = {"origin": "sf_sit", "name": "dspy", "version": {"major": 1, "minor": 0}}
+        root = Root(snowpark_session)
+
+        # fetch service
+        search_service = (
+            root.databases[snowflake_database]
+            .schemas[snowflake_schema]
+            .cortex_search_services[cortex_search_service_name]
+        )
+
+        return search_service
+
+    def _query_cortex_search(self, cortex_search_service, query, columns, filter, k):
+        """Query Cortex Search endpoint for top relevant passages .
 
         Args:
-            query_embeddings(List[float]]): the embeddings for the query of interest
-            doc_table
+            cortex_search_service (object): cortex search service for querying
+            query (str): The query or queries to search for.
+            repsonse_columns: A comma-separated list of columns to return for each relevant result in the response. These columns must be included in the source query for the service.
+            filters: A filter object for filtering results based on data in the ATTRIBUTES columns. See Filter syntax.
             k (Optional[int]): The number of top passages to retrieve. Defaults to self.k.
 
         Returns:
             dspy.Prediction: An object containing the retrieved passages.
         """
-        doc_table_value = self.embeddings_field
-        doc_table_key = self.embeddings_text_field
+        # query service
+        resp = cortex_search_service.search(query=query, columns=columns, filter=filter, limit=k)
 
-        doc_embeddings = self.client.table(self.snowflake_table_name)
-        cosine_similarity = function("vector_cosine_similarity")
-
-        top_k = (
-            doc_embeddings.select(
-                doc_table_value,
-                doc_table_key,
-                cosine_similarity(
-                    doc_embeddings.col(doc_table_value),
-                    lit(query_embeddings).cast(VectorType(float, len(query_embeddings))),
-                ).as_("dist"),
-            )
-            .sort("dist", ascending=False)
-            .limit(k)
-        )
-
-        return top_k.select(doc_table_key).to_pandas().values
-
-    @classmethod
-    def _init_cortex(cls, credentials: dict) -> None:
-        session = Session.builder.configs(credentials).create()
-        session.query_tag = {"origin": "sf_sit", "name": "dspy", "version": {"major": 1, "minor": 0}}
-
-        return session
-
-    def _get_embeddings(self, query: str) -> list[float]:
-        # create embeddings for the query
-        embed = snow_fn.builtin("snowflake.cortex.embed_text_768")
-        cortex_embed_args = embed(snow_fn.lit(self.embeddings_model), snow_fn.lit(query))
-
-        return self.client.range(1).withColumn("complete_cal", cortex_embed_args).collect()[0].COMPLETE_CAL
+        return json.loads(resp.to_json())
