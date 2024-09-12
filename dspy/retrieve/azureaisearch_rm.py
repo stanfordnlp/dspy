fe42601443c7a7122d87c00ccd68f36aa4c9df4f
Jeffrey Mak <44762348+jeffreyrubi@users.noreply.github.com>
2024-09-12 14:58:57 -0400

    Fix search error when doing hybrid search
    get_embedding() is returning List[VectorQueries]. 
    In hybrid-search case, when calling client.search, vector_query should not be passed with extra '[]' which results in parsing error by the AISearch library call.

--- dspy/retrieve/azureaisearch_rm.py
+++ dspy/retrieve/azureaisearch_rm.py
@@ -255,7 +255,7 @@ class AzureAISearchRM(dspy.Retrieve):
                     query_speller=query_speller,
                     semantic_configuration_name=semantic_configuration_name,
                     top=top,
-                    vector_queries=[vector_query],
+                    vector_queries=vector_query,
                     vector_filter_mode=vector_filter_mode,
                     query_caption=("extractive|highlight-false" if use_semantic_captions else None),
                 )
@@ -266,7 +266,7 @@ class AzureAISearchRM(dspy.Retrieve):
                     query_language=query_language,
                     query_speller=query_speller,
                     top=top,
-                    vector_queries=[vector_query],
+                    vector_queries=vector_query,
                     vector_filter_mode=vector_filter_mode,
                 )
         if is_fulltext_search:
