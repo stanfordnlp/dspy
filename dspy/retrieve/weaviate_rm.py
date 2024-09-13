3195ec9594f3e0b667370655500c32a9fb074f8c
Erika Cardenas <110841617+erika-cardenas@users.noreply.github.com>
2024-09-13 17:54:48 -0400

    create object helpers

--- dspy/retrieve/weaviate_rm.py
+++ dspy/retrieve/weaviate_rm.py
@@ -6,6 +6,8 @@ from dspy.primitives.prediction import Prediction
 
 try:
     import weaviate
+    from weaviate.util import get_valid_uuid
+    from uuid import uuid4
 except ImportError as err:
     raise ImportError(
         "The 'weaviate' extra is required to use WeaviateRM. Install it with `pip install dspy-ai[weaviate]`",
