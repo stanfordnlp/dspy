6d4e9526341a469c1ce19124940ef4cdd5a3ca8e
Sri Vardhamanan <sri00001000@gmail.com>
2024-09-18 23:40:48 +0530

    minor cleanup

--- dspy/adapters/base.py
+++ dspy/adapters/base.py
@@ -1,4 +1,16 @@
+import abc
+
+
 class Adapter:
+
+    @abc.abstractmethod
+    def format(self, signature, demos, inputs):
+        pass
+
+    @abc.abstractmethod
+    def parse(self, signature, completion):
+        pass
+
     def __call__(self, lm, lm_kwargs, signature, demos, inputs):
         inputs = self.format(signature, demos, inputs)
         inputs = dict(prompt=inputs) if isinstance(inputs, str) else dict(messages=inputs)
@@ -10,9 +22,11 @@ class Adapter:
             try:
                 value = self.parse(signature, output)
             except Exception as e:
-                print("Failed to parse", messages, output)
+                print("Failed to parse", inputs, output)
                 raise e
-            assert set(value.keys()) == set(signature.output_fields.keys()), f"Expected {signature.output_fields.keys()} but got {value.keys()}"
+            assert set(value.keys()) == set(
+                signature.output_fields.keys()
+            ), f"Expected {signature.output_fields.keys()} but got {value.keys()}"
             values.append(value)
-        
+
         return values
