56a29f6fc5e664d21ca13f8927cf5fcdde3ade18
Omar Khattab <okhat@users.noreply.github.com>
2024-10-08 15:43:35 -0700

    Update setup.py

--- dspy/.internal_dspyai/setup.py
+++ dspy/.internal_dspyai/setup.py
@@ -8,7 +8,7 @@ setup(
     #replace_package_name_marker
     name="dspy-ai",
     #replace_package_version_marker
-    version="2.5.3", 	
+    version="2.5.5", 	
     description="DSPy",	
     long_description=long_description,	
     long_description_content_type="text/markdown",	
