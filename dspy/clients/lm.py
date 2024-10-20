4cf181bc3c155eec462aef495fa8c3f56295fa5f
Zhaohan Dong <65422392+zhaohan-dong@users.noreply.github.com>
2024-10-20 12:10:59 +0000

    Refactoring the litellm functions to take router
    Signed-off-by: Zhaohan Dong <65422392+zhaohan-dong@users.noreply.github.com>

--- dspy/clients/lm.py
+++ dspy/clients/lm.py
@@ -79,7 +79,7 @@ class LM:
         self.history.append(entry)
 
         return outputs
-    
+
     def inspect_history(self, n: int = 1):
         _inspect_history(self, n)
 
@@ -128,7 +128,7 @@ class LM:
         executor.shutdown(wait=False)
 
         return finetune_job
-    
+
     def copy(self, **kwargs):
         """Returns a copy of the language model with possibly updated parameters."""
 
@@ -144,7 +144,6 @@ class LM:
 
         return new_instance
 
-
     def _get_completion_func(self, cache: bool = False):
         if self.model_type == "chat":
             return cached_litellm_completion if cache else litellm_completion
@@ -152,22 +151,68 @@ class LM:
             return cached_litellm_text_completion if cache else litellm_text_completion
 
 
+class RoutedLM(LM):
+    """LM which uses LiteLLM Router to perform completion requests"""
+
+    def __init__(self, model, router, **kwargs):
+        # Type checking that router must be a litellm.Router instance, with model in router.model_names
+        if not isinstance(router, litellm.router.Router):
+            raise TypeError(
+                f"The 'router' argument must be an instance of {litellm.router.Router.__name__}, but received a type '{type(router).__name__}' instead."
+            )
+        # Check if model is supported by the router
+        available_models = router.get_model_names()
+        if model not in available_models:
+            raise ValueError(
+                f"The model '{model}' must be one of the router's model_names. Available models on router: {available_models}"
+            )
+
+        super().__init__(model, **kwargs)
+        self.router = router
+
+    def _get_completion_func(self, cache):
+        if self.model_type == "chat":
+            return lambda request: (
+                cached_litellm_completion(request, self.router)
+                if cache
+                else litellm_completion(request, self.router)
+            )
+        else:
+            return lambda request: (
+                cached_litellm_text_completion(request, self.router)
+                if cache
+                else litellm_text_completion(request, self.router)
+            )
+
+
 @functools.lru_cache(maxsize=None)
-def cached_litellm_completion(request):
-    return litellm_completion(request, cache={"no-cache": False, "no-store": False})
+def cached_litellm_completion(request, router=None):
+    return litellm_completion(
+        request, router=router, cache={"no-cache": False, "no-store": False}
+    )
 
 
-def litellm_completion(request, cache={"no-cache": True, "no-store": True}):
+def litellm_completion(
+    request, router=None, cache={"no-cache": True, "no-store": True}
+):
     kwargs = ujson.loads(request)
-    return litellm.completion(cache=cache, **kwargs)
+    return (
+        litellm.completion(cache=cache, **kwargs)
+        if not router
+        else router.completion(cache=cache, **kwargs)
+    )
 
 
 @functools.lru_cache(maxsize=None)
-def cached_litellm_text_completion(request):
-    return litellm_text_completion(request, cache={"no-cache": False, "no-store": False})
+def cached_litellm_text_completion(request, router=None):
+    return litellm_text_completion(
+        request, router=router, cache={"no-cache": False, "no-store": False}
+    )
 
 
-def litellm_text_completion(request, cache={"no-cache": True, "no-store": True}):
+def litellm_text_completion(
+    request, router=None, cache={"no-cache": True, "no-store": True}
+):
     kwargs = ujson.loads(request)
 
     # Extract the provider and model from the model string.
@@ -175,12 +220,18 @@ def litellm_text_completion(request, cache={"no-cache": True, "no-store": True})
     model = kwargs.pop("model").split("/", 1)
     provider, model = model[0] if len(model) > 1 else "openai", model[-1]
 
+    # Build the prompt from the messages.
+    prompt = "\n\n".join(
+        [x["content"] for x in kwargs.pop("messages")] + ["BEGIN RESPONSE:"]
+    )
+
+    # Send request to router if router is not None
+    if router:
+        return router.text_completion(cache=cache, model=model, prompt=prompt, **kwargs)
+
     # Use the API key and base from the kwargs, or from the environment.
     api_key = kwargs.pop("api_key", None) or os.getenv(f"{provider}_API_KEY")
     api_base = kwargs.pop("api_base", None) or os.getenv(f"{provider}_API_BASE")
-
-    # Build the prompt from the messages.
-    prompt = "\n\n".join([x["content"] for x in kwargs.pop("messages")] + ["BEGIN RESPONSE:"])
 
     return litellm.text_completion(
         cache=cache,
@@ -224,66 +275,3 @@ def _inspect_history(lm, n: int = 1):
             print(_red(choices_text, end=""))
 
     print("\n\n\n")
-
-
-class RoutedLM(LM):
-    """LM which uses LiteLLM Router to perform completion requests"""
-
-    def __init__(self, model, router, **kwargs):
-        # Type checking that router must be a litellm.Router instance, with model in router.model_names
-        if not isinstance(router, litellm.router.Router):
-            raise TypeError(
-                f"The 'router' argument must be an instance of {litellm.router.Router.__name__}, but received a type '{type(router).__name__}' instead."
-            )
-        # Check if model is supported by the router
-        available_models = router.get_model_names()
-        if model not in available_models:
-            raise ValueError(
-                f"The model '{model}' must be one of the router's model_names. Available models on router: {available_models}"
-            )
-
-        super().__init__(model, **kwargs)
-        self.router = router
-
-    def _get_completion_func(self, cache):
-        if self.model_type == "chat":
-            return self._cached_router_completion if cache else self._router_completion
-        else:
-            return (
-                self._cached_router_text_completion
-                if cache
-                else self._router_text_completion
-            )
-
-    @functools.lru_cache(maxsize=None)
-    def _cached_router_completion(self, request):
-        """Cache-enabled completion method that uses the router."""
-        return self._router_completion(
-            request, cache={"no-cache": False, "no-store": False}
-        )
-
-    def _router_completion(self, request, cache={"no-cache": True, "no-store": True}):
-        """Actual completion logic using the router."""
-        kwargs = ujson.loads(request)
-        return self.router.completion(cache=cache, **kwargs)
-
-    @functools.lru_cache(maxsize=None)
-    def _cached_router_text_completion(self, request):
-        return self._router_text_completion(
-            request, cache={"no-cache": False, "no-store": False}
-        )
-
-    def _router_text_completion(
-        self, request, cache={"no-cache": True, "no-store": True}
-    ):
-        kwargs = ujson.loads(request)
-
-        # The model alias for litellm.Router assigned by user, not the official model name
-        model_name = kwargs.pop("model")
-        prompt = "\n\n".join(
-            [x["content"] for x in kwargs.pop("messages")] + ["BEGIN RESPONSE:"]
-        )
-
-        return self.router.text_completion(
-            cache=cache, model=model_name, prompt=prompt, **kwargs
-        )
