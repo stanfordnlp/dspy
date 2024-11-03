88ed4e700f160aeb924fdb86a41cdae5ce9e9a29
Isaac Miller <17116851+isaacbmiller@users.noreply.github.com>
2024-11-03 11:21:43 -0600

    Delete examples/temp.py

--- examples/temp.py
+++
@@ -1,9 +0,0 @@
-import dspy
-
-lm = dspy.LM("gpt-4o-mini")
-dspy.settings.configure(lm=lm)
-
-predictor = dspy.Predict("query: str, image: Image -> response: str")
-predictor(query="What is this dog?", image=dspy.Image.from_url("https://images.dog.ceo/breeds/dane-great/n02109047_8912.jpg"))
-
-dspy.inspect_history()
