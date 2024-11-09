d69ec390a4064c70e8803c26882fb5aab65338c2
Chen Qian <chen.qian@databricks.com>
2024-11-08 17:11:22 -0800

    change the way of enable/disable litellm logging (#1777)

--- dspy/clients/__init__.py
+++ dspy/clients/__init__.py
@@ -10,7 +10,15 @@ from litellm.caching import Cache
 DISK_CACHE_DIR = os.environ.get("DSPY_CACHEDIR") or os.path.join(Path.home(), ".dspy_cache")
 litellm.cache = Cache(disk_cache_dir=DISK_CACHE_DIR, type="disk")
 litellm.telemetry = False
+# Turn off by default to avoid LiteLLM logging during every LM call.
+litellm.suppress_debug_info = True
 
 if "LITELLM_LOCAL_MODEL_COST_MAP" not in os.environ:
-    # accessed at run time by litellm; i.e., fine to keep after import
+    # Accessed at run time by litellm; i.e., fine to keep after import
     os.environ["LITELLM_LOCAL_MODEL_COST_MAP"] = "True"
+
+def enable_litellm_logging():
+    litellm.suppress_debug_info = False
+
+def disable_litellm_logging():
+    litellm.suppress_debug_info = True
