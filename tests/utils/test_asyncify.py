ddeeec94bf73181be262f15b51c0642178c52091
chenmoneygithub <chen.qian@databricks.com>
2024-11-15 17:02:27 -0800

    deflake

--- tests/utils/test_asyncify.py
+++ tests/utils/test_asyncify.py
@@ -1,6 +1,7 @@
 from time import time, sleep
 import asyncio
 
+import math
 import pytest
 
 import dspy
@@ -30,17 +31,25 @@ async def test_asyncify():
     async def run_n_tasks(n: int, wait: float):
         await asyncio.gather(*[ask_the_question(wait) for _ in range(n)])
 
-    async def assert_capacity(capacity: int, wait: float = 0.01, buffer: float = 0.1):
+    async def verify_asyncify(capacity: int, number_of_tasks: int, wait: float = 0.5):
         dspy.settings.configure(async_max_workers=capacity)
-        t1 = time()
-        await run_n_tasks(capacity, wait)
-        t2 = time()
-        assert t2 - t1 < wait + buffer
 
-        t3 = time()
-        await run_n_tasks(capacity * 1, wait)
-        t4 = time()
-        assert t4 - t3 < (wait + buffer) * 2
+        start = time()
+        await run_n_tasks(number_of_tasks, wait)
+        end = time()
+        total_time = end - start
 
-    for n in [1, 2, 4, 8, 16, 32, 64]:
-        await assert_capacity(n)
+        # If asyncify is working correctly, the total time should be less than the total number of loops
+        # `(number_of_tasks / capacity)` times wait time, plus the computational overhead. The lower bound should
+        # be `math.floor(number_of_tasks * 1.0 / capacity) * wait` because there are more than
+        # `math.floor(number_of_tasks * 1.0 / capacity)` loops.
+        lower_bound = math.floor(number_of_tasks * 1.0 / capacity) * wait
+        upper_bound = math.ceil(number_of_tasks * 1.0 / capacity) * wait + 2 * wait  # 2*wait for buffer
+
+        assert lower_bound < total_time < upper_bound
+
+    await verify_asyncify(4, 10)
+    await verify_asyncify(8, 15)
+    await verify_asyncify(8, 30)
+
+
