f0efe4549b167b46a6edf046f3591aa188dc932f
Omar Khattab <okhat@users.noreply.github.com>
2024-11-23 22:24:54 -0800

    Wrap disk_cache.size_limit adjustment in an if stmt

--- dspy/clients/__init__.py
+++ dspy/clients/__init__.py
@@ -13,7 +13,9 @@ DISK_CACHE_LIMIT = int(os.environ.get("DSPY_CACHE_LIMIT", 3e10))  # 30 GB defaul
 # TODO: There's probably value in getting litellm to support FanoutCache and to separate the limit for
 # the LM cache from the embeddings cache. Then we can lower the default 30GB limit.
 litellm.cache = Cache(disk_cache_dir=DISK_CACHE_DIR, type="disk")
-litellm.cache.cache.disk_cache.reset('size_limit', DISK_CACHE_LIMIT)
+
+if litellm.cache.cache.disk_cache.size_limit != DISK_CACHE_LIMIT:
+    litellm.cache.cache.disk_cache.reset('size_limit', DISK_CACHE_LIMIT)
 
 litellm.telemetry = False
 
