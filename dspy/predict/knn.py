2386fc3a202310ff58277567cfa91bb5e57a6db5
Cyrus Nouroozi <cyrus@zenbase.ai>
2024-12-04 08:21:17 -0600

    Add lazy vectorization to dspy.KNN

--- dspy/predict/knn.py
+++ dspy/predict/knn.py
@@ -1,3 +1,4 @@
+from concurrent.futures import ThreadPoolExecutor
 from typing import List, TYPE_CHECKING
 
 import numpy as np
@@ -9,7 +10,7 @@ if TYPE_CHECKING:
 
 
 class KNN:
-    def __init__(self, k: int, trainset: List["dspy.Example"], vectorizer=None):
+    def __init__(self, k: int, trainset: List["dspy.Example"], vectorizer=None, lazy=True):
         """
         A k-nearest neighbors retriever that finds similar examples from a training set.
 
@@ -28,6 +29,10 @@ class KNN:
         self.k = k
         self.trainset = trainset
         self.embedding = vectorizer or dspy.Embedder(dsp.SentenceTransformersVectorizer())
+        if not lazy:
+            self.embed()
+
+    def embed(self):
         trainset_casted_to_vectorize = [
             " | ".join([f"{key}: {value}" for key, value in example.items() if key in example._input_keys])
             for example in self.trainset
@@ -35,8 +40,21 @@ class KNN:
         self.trainset_vectors = self.embedding(trainset_casted_to_vectorize).astype(np.float32)
 
     def __call__(self, **kwargs) -> List["dspy.Example"]:
+        if not hasattr(self, "trainset_vectors"):
+            self.embed()
+
         input_example_vector = self.embedding([" | ".join([f"{key}: {val}" for key, val in kwargs.items()])])
         scores = np.dot(self.trainset_vectors, input_example_vector.T).squeeze()
         nearest_samples_idxs = scores.argsort()[-self.k :][::-1]
         train_sampled = [self.trainset[cur_idx] for cur_idx in nearest_samples_idxs]
         return train_sampled
+
+
+def load_knn_embeddings(program, num_threads=6):
+    knns = [p.retrieve_demos for p in program.predictors() if hasattr(p, "retrieve_demos")]
+
+    def do_hydrate(knn):
+        knn.embed()
+
+    with ThreadPoolExecutor(max_workers=num_threads) as executor:
+        list(executor.map(do_hydrate, knns))
