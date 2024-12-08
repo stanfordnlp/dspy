4c4bc2915442e71eb8ef785c49e385432d49f119
Corey Zumar <39497902+dbczumar@users.noreply.github.com>
2024-12-08 05:16:08 -0800

    Caching: Use reentrant locks, don't discard callables (or any other unhashable object in the future) from the cache key (#1905)
    * fix
    
    Signed-off-by: dbczumar <corey.zumar@databricks.com>
    
    * fix
    
    Signed-off-by: dbczumar <corey.zumar@databricks.com>
    
    * fix
    
    Signed-off-by: dbczumar <corey.zumar@databricks.com>
    
    * fix
    
    Signed-off-by: dbczumar <corey.zumar@databricks.com>
    
    * Fallback
    
    Signed-off-by: dbczumar <corey.zumar@databricks.com>
    
    * fix
    
    Signed-off-by: dbczumar <corey.zumar@databricks.com>
    
    * fix
    
    Signed-off-by: dbczumar <corey.zumar@databricks.com>
    
    * fix
    
    Signed-off-by: dbczumar <corey.zumar@databricks.com>
    
    * fix
    
    Signed-off-by: dbczumar <corey.zumar@databricks.com>
    
    * fix
    
    Signed-off-by: dbczumar <corey.zumar@databricks.com>
    
    * fix
    
    Signed-off-by: dbczumar <corey.zumar@databricks.com>
    
    * fix
    
    Signed-off-by: dbczumar <corey.zumar@databricks.com>
    
    * fix
    
    Signed-off-by: dbczumar <corey.zumar@databricks.com>
    
    * fix
    
    Signed-off-by: dbczumar <corey.zumar@databricks.com>
    
    * Test
    
    Signed-off-by: dbczumar <corey.zumar@databricks.com>
    
    * fix
    
    Signed-off-by: dbczumar <corey.zumar@databricks.com>
    
    * Debug
    
    Signed-off-by: dbczumar <corey.zumar@databricks.com>
    
    * Debug
    
    Signed-off-by: dbczumar <corey.zumar@databricks.com>
    
    * Debug
    
    Signed-off-by: dbczumar <corey.zumar@databricks.com>
    
    * fix
    
    Signed-off-by: dbczumar <corey.zumar@databricks.com>
    
    * progress
    
    Signed-off-by: dbczumar <corey.zumar@databricks.com>
    
    * Test no cache
    
    Signed-off-by: dbczumar <corey.zumar@databricks.com>
    
    * Test no cache
    
    Signed-off-by: dbczumar <corey.zumar@databricks.com>
    
    * fix
    
    Signed-off-by: dbczumar <corey.zumar@databricks.com>
    
    ---------
    
    Signed-off-by: dbczumar <corey.zumar@databricks.com>

--- tests/clients/test_lm.py
+++ tests/clients/test_lm.py
@@ -1,5 +1,6 @@
 from unittest import mock
 
+import litellm
 import pydantic
 import pytest
 
@@ -49,21 +50,29 @@ def test_text_lms_can_be_queried(litellm_test_server):
     assert azure_openai_lm("azure openai query") == expected_response
 
 
-def test_lm_calls_support_unhashable_types(litellm_test_server):
-    api_base, server_log_file_path = litellm_test_server
+def test_lm_calls_support_callables(litellm_test_server):
+    api_base, _ = litellm_test_server
 
-    lm_with_unhashable_callable = dspy.LM(
-        model="openai/dspy-test-model",
-        api_base=api_base,
-        api_key="fakekey",
-        # Define a callable kwarg for the LM to use during inference
-        azure_ad_token_provider=lambda *args, **kwargs: None,
-    )
-    lm_with_unhashable_callable("Query")
+    with mock.patch("litellm.completion", autospec=True, wraps=litellm.completion) as spy_completion:
+        azure_ad_token_provider = lambda *args, **kwargs: None
+        lm_with_callable = dspy.LM(
+            model="openai/dspy-test-model",
+            api_base=api_base,
+            api_key="fakekey",
+            azure_ad_token_provider=azure_ad_token_provider,
+        )
+        lm_with_callable("Query")
+
+        spy_completion.assert_called_once()
+        call_args = spy_completion.call_args.kwargs
+        assert call_args["model"] == "openai/dspy-test-model"
+        assert call_args["api_base"] == api_base
+        assert call_args["api_key"] == "fakekey"
+        assert call_args["azure_ad_token_provider"] is azure_ad_token_provider
 
 
 def test_lm_calls_support_pydantic_models(litellm_test_server):
-    api_base, server_log_file_path = litellm_test_server
+    api_base, _ = litellm_test_server
 
     class ResponseFormat(pydantic.BaseModel):
         response: str
