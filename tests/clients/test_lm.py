e77f143cc768bd444f233972031701b3ab10a607
dbczumar <corey.zumar@databricks.com>
2024-12-08 02:38:01 -0800

    fix
    Signed-off-by: dbczumar <corey.zumar@databricks.com>

--- tests/clients/test_lm.py
+++ tests/clients/test_lm.py
@@ -1,10 +1,11 @@
 from unittest import mock
 
+import litellm
 import pydantic
 import pytest
 
 import dspy
-from tests.test_utils.server import litellm_test_server, read_litellm_test_server_request_logs
+from tests.test_utils.server import litellm_test_server
 
 
 def test_chat_lms_can_be_queried(litellm_test_server):
@@ -50,32 +51,24 @@ def test_text_lms_can_be_queried(litellm_test_server):
 
 
 def test_lm_calls_support_callables(litellm_test_server):
-    api_base, server_log_file_path = litellm_test_server
+    api_base, _ = litellm_test_server
 
-    lm_with_callable = dspy.LM(
-        model="openai/dspy-test-model",
-        api_base=api_base,
-        api_key="fakekey",
-        # Define a callable kwarg for the LM to use during inference
-        azure_ad_token_provider=lambda *args, **kwargs: None,
-    )
-    # Invoke the LM twice; the second call should be cached in memory
-    lm_with_callable("Callable test query")
-    # lm_with_callable("Callable test query")
+    with mock.patch("litellm.completion", autospec=True, wraps=litellm.completion) as spy_completion:
+        azure_ad_token_provider = lambda *args, **kwargs: None
+        lm_with_callable = dspy.LM(
+            model="openai/dspy-test-model",
+            api_base=api_base,
+            api_key="fakekey",
+            azure_ad_token_provider=azure_ad_token_provider,
+        )
+        lm_with_callable("Query")
 
-    # Define and invoke a nearly-identical LM that lacks the callable kwarg
-    lm_without_callable = dspy.LM(
-        model="openai/dspy-test-model",
-        api_base=api_base,
-        api_key="fakekey",
-    )
-    lm_without_callable("Callable test query")
-
-    # Verify that 2 requests were made to the LiteLLM server - one for each LM.
-    # This verifies that there wasn't a cache collision between the LMs due to
-    # the callable
-    request_logs = read_litellm_test_server_request_logs(server_log_file_path)
-    assert len(request_logs) == 2
+        spy_completion.assert_called_once()
+        call_args = spy_completion.call_args.kwargs
+        assert call_args["model"] == "openai/dspy-test-model"
+        assert call_args["api_base"] == api_base
+        assert call_args["api_key"] == "fakekey"
+        assert call_args["azure_ad_token_provider"] is azure_ad_token_provider
 
 
 def test_lm_calls_support_pydantic_models(litellm_test_server):
