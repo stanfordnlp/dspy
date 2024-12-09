e0101d1bf55ba6e76d51a6128dfe661293fb183a
Thomas D Ahle <thomas@ahle.dk>
2024-12-09 21:42:09 +0100

    Missing Image import

--- dspy/signatures/signature.py
+++ dspy/signatures/signature.py
@@ -13,6 +13,7 @@ from pydantic import BaseModel, Field, create_model
 from pydantic.fields import FieldInfo
 
 import dsp
+from dspy.adapters.image_utils import Image
 from dspy.signatures.field import InputField, OutputField, new_to_old_field
 
 
