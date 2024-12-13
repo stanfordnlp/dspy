4d914765f3828795529af30c2da8e2bc06b1a8e4
Omar Khattab <okhat@users.noreply.github.com>
2024-12-13 08:02:12 -0800

    Remove old caches and deprecate pre-2.5 examples (#1935)

--- cache/joblib/dsp/modules/hf_client/send_hftgi_request_v01_wrapped/func_code.py
+++
@@ -1,4 +0,0 @@
-# first line: 85
-@NotebookCacheMemory.cache(ignore=['arg'])
-def send_hftgi_request_v01_wrapped(arg, url, ports, **kwargs):
-    return send_hftgi_request_v01(arg, url, ports, **kwargs)
