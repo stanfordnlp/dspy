10c7bbde4d63542cad634e8faec41debe2cfdb8e
dbczumar <corey.zumar@databricks.com>
2024-12-16 20:09:44 -0800

    test streaming
    Signed-off-by: dbczumar <corey.zumar@databricks.com>

---
+++ tests/utils/test_streaming.py
@@ -0,0 +1,49 @@
+import dspy
+from dspy.utils.streaming import streaming_response
+
+import pytest
+
+from tests.test_utils.server import litellm_test_server
+
+
+@pytest.mark.asyncio
+async def test_streamify_yields_expected_response_chunks(litellm_test_server):
+    api_base, _ = litellm_test_server
+    lm = dspy.LM(
+        model="openai/dspy-test-model",
+        api_base=api_base,
+        api_key="fakekey",
+    )
+    with dspy.context(lm=lm):
+        class TestSignature(dspy.Signature):
+            input_text: str = dspy.InputField()
+            output_text: str = dspy.OutputField()
+
+        program = dspy.streamify(dspy.Predict(TestSignature))
+        output_stream = program(input_text="Test")
+        output_chunks = [chunk async for chunk in output_stream]
+        last_chunk = output_chunks[-1]
+        assert isinstance(last_chunk, dspy.Prediction)
+        assert last_chunk.output_text == "Hello!"
+
+
+@pytest.mark.asyncio
+async def test_streaming_response_yields_expected_response_chunks(litellm_test_server):
+    api_base, _ = litellm_test_server
+    lm = dspy.LM(
+        model="openai/dspy-test-model",
+        api_base=api_base,
+        api_key="fakekey",
+    )
+    with dspy.context(lm=lm):
+        class TestSignature(dspy.Signature):
+            input_text: str = dspy.InputField()
+            output_text: str = dspy.OutputField()
+
+        program = dspy.streamify(dspy.Predict(TestSignature))
+        output_stream_from_program = streaming_response(program(input_text="Test"))
+        output_stream_for_server_response = streaming_response(output_stream_from_program)
+        output_chunks = [chunk async for chunk in output_stream_for_server_response]
+        assert all(chunk.startswith("data: ") for chunk in output_chunks)
+        assert 'data: {"prediction":{"output_text":"Hello!"}}\n\n' in output_chunks
+        assert output_chunks[-1] == "data: [DONE]\n\n"
