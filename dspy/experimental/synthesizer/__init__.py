51e999db40f1dde35bd72e78d37086ef88a49288
chenmoneygithub <chen.qian@databricks.com>
2024-12-23 14:00:49 -0800

    add css and yml config

--- dspy/experimental/synthesizer/__init__.py
+++ dspy/experimental/synthesizer/__init__.py
@@ -1,8 +1,1 @@
-from dspy.experimental.synthesizer.synthesizer import Synthesizer
-from dspy.experimental.synthesizer.config import SynthesizerArguments
-
-
-__all__ = [
-    "Synthesizer",
-    "SynthesizerArguments",
-]
+from dspy.experimental.synthesizer import *
