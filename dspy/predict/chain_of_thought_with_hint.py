4f2d2f826079288944ccc2172097cb3b862cf951
Chen Qian <chen.qian@databricks.com>
2025-02-23 22:30:31 -0800

    Introduce `dspy.Refine`, `dspy.BestOfN` and fix thread safety for `dspy.ChainOfThoughtWithHint` (#1959)
    * Add DSPy Refine
    
    * Introduce BestOfN and make Refine & CoTWithHint thread safe
    
    * Improve Refine implementation
    
    ---------
    
    Co-authored-by: Omar Khattab <okhattab@stanford.edu>
    Co-authored-by: Omar Khattab <okhat@users.noreply.github.com>

--- dspy/predict/chain_of_thought_with_hint.py
+++ dspy/predict/chain_of_thought_with_hint.py
@@ -1,8 +1,5 @@
 import dspy
-
 from .predict import Module
-
-# TODO: FIXME: Insert this right before the *first* output field. Also rewrite this to use the new signature system.
 
 class ChainOfThoughtWithHint(Module):
     def __init__(self, signature, rationale_type=None, **config):
@@ -19,16 +16,11 @@ class ChainOfThoughtWithHint(Module):
             kwargs[last_key] = str(kwargs[last_key]) + hint
 
             # Run CoT then update the trace with original kwargs, i.e. without the hint.
-            pred = self.module(**kwargs)
-            this_trace = dspy.settings.trace[-1]
-            dspy.settings.trace[-1] = (this_trace[0], original_kwargs, this_trace[2])
+            with dspy.context(trace=[]):
+                pred = self.module(**kwargs)
+                this_trace = dspy.settings.trace[-1]
+
+            dspy.settings.trace.append((this_trace[0], original_kwargs, this_trace[2]))
             return pred
         
         return self.module(**kwargs)
-
-
-"""
-TODO: In principle, we can update the field's prefix during forward too to fill any thing based on the input args.
-
-IF the user didn't overwrite our default rationale_type.
-"""
