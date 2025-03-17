540d69131d53f43689ead924498282683699491f
TomuHirata <tomu.hirata@gmail.com>
2025-03-17 23:51:48 +0900

    define compile and get_params to Teleprompter (#7973)

---
+++ tests/teleprompt/test_teleprompt.py
@@ -0,0 +1,19 @@
+from dspy.teleprompt.teleprompt import Teleprompter
+
+class DummyTeleprompter(Teleprompter):
+    def __init__(self, param1: int, param2: str):
+        super().__init__()
+        self.param1 = param1
+        self.param2 = param2
+
+    def compile(self, student, *, trainset, teacher=None, valset=None, **kwargs):
+        return student
+
+    def get_params(self):
+        return self.__dict__
+
+
+def test_get_params():
+    teleprompter = DummyTeleprompter(param1=1, param2="test")
+    params = teleprompter.get_params()
+    assert params == {"param1": 1, "param2": "test"}
