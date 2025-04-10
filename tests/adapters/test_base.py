181ace4db7b31ad5e5bce047b1b919b8c4e65a9d
TomuHirata <tomu.hirata@gmail.com>
2025-04-10 12:36:05 +0900

    add demos to tests

--- tests/adapters/test_base.py
+++ tests/adapters/test_base.py
@@ -7,20 +7,34 @@ from dspy.adapters.base import Adapter
 from dspy.signatures.signature import Signature
 from dspy.utils.dummies import DummyLM
 
+
 class MockSignature(Signature):
     question: str = dspy.InputField()
     answer: int = dspy.OutputField()
 
-def test_adapter_retry_logic():
+
+@pytest.mark.parametrize(
+    "demos",
+    [
+        [],
+        [dspy.Example({"question": "6 x 7", "answer": 42})],
+    ],
+)
+def test_adapter_retry_logic(demos):
     adapter = Adapter()
     lm = DummyLM([{"answer": "42"}])
-    demos = []
     inputs = {"question": "6 x 7"}
 
     with dspy.context(adapter_retry_count=1, lm=lm):
         # Mock parse to raise ValidationError on the first call and succeed on the second
-        with patch.object(adapter, "format", return_value="formatted_input") as mock_format, \
-            patch.object(adapter, "parse", side_effect=lambda signature, completion: TypeAdapter(signature).validate_python(completion)) as mock_parse:
+        with (
+            patch.object(adapter, "format", return_value="formatted_input") as mock_format,
+            patch.object(
+                adapter,
+                "parse",
+                side_effect=lambda signature, completion: TypeAdapter(signature).validate_python(completion),
+            ) as mock_parse,
+        ):
             with pytest.raises(ValidationError):
                 adapter(lm, {}, MockSignature, demos, inputs)
 
