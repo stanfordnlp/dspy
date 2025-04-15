00a409db091d8829d8e0322d975ce2b90072bf82
TomuHirata <tomu.hirata@gmail.com>
2025-04-15 16:08:32 +0900

    move num_threads into global settings

--- dspy/dsp/utils/settings.py
+++ dspy/dsp/utils/settings.py
@@ -23,6 +23,7 @@ DEFAULT_CONFIG = dotdict(
     track_usage=False,
     usage_tracker=None,
     provide_traceback=False, # Whether to include traceback information in error logs.
+    num_threads=8, # Number of threads to use for parallel processing.
 )
 
 # Global base configuration and owner tracking
