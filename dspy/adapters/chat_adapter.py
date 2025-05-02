c98bbad09709583c7c85e4ad634f049d8e7ebd92
chenmoneygithub <chen.qian@databricks.com>
2025-05-01 17:44:05 -0700

    remove extra

--- dspy/adapters/chat_adapter.py
+++ dspy/adapters/chat_adapter.py
@@ -170,7 +170,7 @@ class ChatAdapter(Adapter):
                 except Exception as e:
                     raise ValueError(
                         f"Error parsing field {k}: {e}.\n\n\t\tOn attempting to parse the value\n```\n{v}\n```"
-                    ) from e
+                    )
         if fields.keys() != signature.output_fields.keys():
             raise ValueError(f"Expected {signature.output_fields.keys()} but got {fields.keys()}")
 
