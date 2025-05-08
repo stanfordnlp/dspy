858c3b120071e9cfdeb751ae0e7e9ddfe0520b91
chenmoneygithub <chen.qian@databricks.com>
2025-05-08 15:05:17 -0700

    fix test

--- tests/predict/test_parallel.py
+++ tests/predict/test_parallel.py
@@ -4,13 +4,15 @@ from dspy.utils.dummies import DummyLM
 
 
 def test_parallel_module():
-    lm = DummyLM([
-        {"output": "test output 1"},
-        {"output": "test output 2"},
-        {"output": "test output 3"},
-        {"output": "test output 4"},
-        {"output": "test output 5"},
-    ])
+    lm = DummyLM(
+        [
+            {"output": "test output 1"},
+            {"output": "test output 2"},
+            {"output": "test output 3"},
+            {"output": "test output 4"},
+            {"output": "test output 5"},
+        ]
+    )
     dspy.settings.configure(lm=lm)
 
     class MyModule(dspy.Module):
@@ -22,13 +24,15 @@ def test_parallel_module():
             self.parallel = dspy.Parallel(num_threads=2)
 
         def forward(self, input):
-            return self.parallel([
-                (self.predictor, input),
-                (self.predictor2, input),
-                (self.predictor, input),
-                (self.predictor2, input),
-                (self.predictor, input),
-            ])
+            return self.parallel(
+                [
+                    (self.predictor, input),
+                    (self.predictor2, input),
+                    (self.predictor, input),
+                    (self.predictor2, input),
+                    (self.predictor, input),
+                ]
+            )
 
     output = MyModule()(dspy.Example(input="test input").with_inputs("input"))
 
@@ -37,20 +41,24 @@ def test_parallel_module():
 
 
 def test_batch_module():
-    lm = DummyLM([
-        {"output": "test output 1"},
-        {"output": "test output 2"},
-        {"output": "test output 3"},
-        {"output": "test output 4"},
-        {"output": "test output 5"},
-    ])
-    res_lm = DummyLM([
-        {"output": "test output 1", "reasoning": "test reasoning 1"},
-        {"output": "test output 2", "reasoning": "test reasoning 2"},
-        {"output": "test output 3", "reasoning": "test reasoning 3"},
-        {"output": "test output 4", "reasoning": "test reasoning 4"},
-        {"output": "test output 5", "reasoning": "test reasoning 5"},
-    ])
+    lm = DummyLM(
+        [
+            {"output": "test output 1"},
+            {"output": "test output 2"},
+            {"output": "test output 3"},
+            {"output": "test output 4"},
+            {"output": "test output 5"},
+        ]
+    )
+    res_lm = DummyLM(
+        [
+            {"output": "test output 1", "reasoning": "test reasoning 1"},
+            {"output": "test output 2", "reasoning": "test reasoning 2"},
+            {"output": "test output 3", "reasoning": "test reasoning 3"},
+            {"output": "test output 4", "reasoning": "test reasoning 4"},
+            {"output": "test output 5", "reasoning": "test reasoning 5"},
+        ]
+    )
 
     class MyModule(dspy.Module):
         def __init__(self):
@@ -61,11 +69,11 @@ def test_batch_module():
             self.parallel = dspy.Parallel(num_threads=2)
 
         def forward(self, input):
-            dspy.settings.configure(lm=lm)
-            res1 = self.predictor.batch([input] * 5)
+            with dspy.context(lm=lm):
+                res1 = self.predictor.batch([input] * 5)
 
-            dspy.settings.configure(lm=res_lm)
-            res2 = self.predictor2.batch([input] * 5)
+            with dspy.context(lm=res_lm):
+                res2 = self.predictor2.batch([input] * 5)
 
             return (res1, res2)
 
@@ -83,13 +91,15 @@ def test_batch_module():
 
 
 def test_nested_parallel_module():
-    lm = DummyLM([
-        {"output": "test output 1"},
-        {"output": "test output 2"},
-        {"output": "test output 3"},
-        {"output": "test output 4"},
-        {"output": "test output 5"},
-    ])
+    lm = DummyLM(
+        [
+            {"output": "test output 1"},
+            {"output": "test output 2"},
+            {"output": "test output 3"},
+            {"output": "test output 4"},
+            {"output": "test output 5"},
+        ]
+    )
     dspy.settings.configure(lm=lm)
 
     class MyModule(dspy.Module):
@@ -101,14 +111,19 @@ def test_nested_parallel_module():
             self.parallel = dspy.Parallel(num_threads=2)
 
         def forward(self, input):
-            return self.parallel([
-                (self.predictor, input),
-                (self.predictor2, input),
-                (self.parallel, [
-                    (self.predictor2, input),
+            return self.parallel(
+                [
                     (self.predictor, input),
-                ]),
-            ])
+                    (self.predictor2, input),
+                    (
+                        self.parallel,
+                        [
+                            (self.predictor2, input),
+                            (self.predictor, input),
+                        ],
+                    ),
+                ]
+            )
 
     output = MyModule()(dspy.Example(input="test input").with_inputs("input"))
 
@@ -120,13 +135,15 @@ def test_nested_parallel_module():
 
 
 def test_nested_batch_method():
-    lm = DummyLM([
-        {"output": "test output 1"},
-        {"output": "test output 2"},
-        {"output": "test output 3"},
-        {"output": "test output 4"},
-        {"output": "test output 5"},
-    ])
+    lm = DummyLM(
+        [
+            {"output": "test output 1"},
+            {"output": "test output 2"},
+            {"output": "test output 3"},
+            {"output": "test output 4"},
+            {"output": "test output 5"},
+        ]
+    )
     dspy.settings.configure(lm=lm)
 
     class MyModule(dspy.Module):
@@ -135,11 +152,15 @@ def test_nested_batch_method():
             self.predictor = dspy.Predict("input -> output")
 
         def forward(self, input):
-            res = self.predictor.batch([dspy.Example(input=input).with_inputs("input")]*2)
+            res = self.predictor.batch([dspy.Example(input=input).with_inputs("input")] * 2)
 
             return res
 
-    result = MyModule().batch([dspy.Example(input="test input").with_inputs("input")]*2)
+    result = MyModule().batch([dspy.Example(input="test input").with_inputs("input")] * 2)
 
-    assert {result[0][0].output, result[0][1].output, result[1][0].output, result[1][1].output} \
-            == {"test output 1", "test output 2", "test output 3", "test output 4"}
+    assert {result[0][0].output, result[0][1].output, result[1][0].output, result[1][1].output} == {
+        "test output 1",
+        "test output 2",
+        "test output 3",
+        "test output 4",
+    }
