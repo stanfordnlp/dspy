8dda2a2a462ae9cae233e0d53fb0b5d4c9ac2904
Benjamin Tripp <71607838+BTripp1986@users.noreply.github.com>
2025-05-07 22:12:51 -0400

    fix spelling typo in react module (#8191)

--- dspy/predict/react.py
+++ dspy/predict/react.py
@@ -41,7 +41,7 @@ class ReAct(Module):
         tools["finish"] = Tool(
             func=lambda: "Completed.",
             name="finish",
-            desc=f"Marks the task as complete. That is, signals that all infomration for producing the outputs, i.e. {outputs}, are now available to be extracted.",
+            desc=f"Marks the task as complete. That is, signals that all information for producing the outputs, i.e. {outputs}, are now available to be extracted.",
             args={},
         )
 
