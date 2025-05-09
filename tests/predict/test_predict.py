ede64ff8476cd3ec86e0a046931d4e8748d938e4
chenmoneygithub <chen.qian@databricks.com>
2025-05-09 16:46:32 -0700

    track usage async test

--- tests/predict/test_predict.py
+++ tests/predict/test_predict.py
@@ -6,6 +6,8 @@ import pydantic
 import pytest
 import ujson
 import os
+import time
+import asyncio
 
 import dspy
 from dspy import Predict, Signature
@@ -509,6 +511,12 @@ def test_lm_usage():
 
 def test_lm_usage_with_parallel():
     program = Predict("question -> answer")
+
+    def program_wrapper(question):
+        # Sleep to make it possible to cause a race condition
+        time.sleep(0.5)
+        return program(question=question)
+
     dspy.settings.configure(lm=dspy.LM("openai/gpt-4o-mini", cache=False), track_usage=True)
     with patch(
         "dspy.clients.lm.litellm_completion",
@@ -519,10 +527,39 @@ def test_lm_usage_with_parallel():
     ):
         parallelizer = dspy.Parallel()
         input_pairs = [
-            (program, {"question": "What is the capital of France?"}),
-            (program, {"question": "What is the capital of France?"}),
+            (program_wrapper, {"question": "What is the capital of France?"}),
+            (program_wrapper, {"question": "What is the capital of France?"}),
         ]
         results = parallelizer(input_pairs)
+        assert results[0].answer == "Paris"
+        assert results[1].answer == "Paris"
+        assert results[0].get_lm_usage()["openai/gpt-4o-mini"]["total_tokens"] == 10
+        assert results[1].get_lm_usage()["openai/gpt-4o-mini"]["total_tokens"] == 10
+
+
+@pytest.mark.asyncio
+async def test_lm_usage_with_async():
+    program = Predict("question -> answer")
+
+    async def program_wrapper(question):
+        # Sleep to make it possible to cause a race condition
+        await asyncio.sleep(0.5)
+        return await program.acall(question=question)
+
+    dspy.settings.configure(lm=dspy.LM("openai/gpt-4o-mini", cache=False), track_usage=True)
+    with patch(
+        "litellm.acompletion",
+        return_value=ModelResponse(
+            choices=[{"message": {"content": "[[ ## answer ## ]]\nParis"}}],
+            usage={"total_tokens": 10},
+        ),
+    ):
+        tasks = []
+        async with asyncio.TaskGroup() as tg:
+            tasks.append(tg.create_task(program_wrapper(question="What is the capital of France?")))
+            tasks.append(tg.create_task(program_wrapper(question="What is the capital of France?")))
+
+        results = await asyncio.gather(*tasks)
         assert results[0].answer == "Paris"
         assert results[1].answer == "Paris"
         assert results[0].get_lm_usage()["openai/gpt-4o-mini"]["total_tokens"] == 10
--- tests/utils/test_settings.py
+++ tests/utils/test_settings.py
@@ -4,11 +4,12 @@ from litellm import ModelResponse, Choices, Message
 from unittest import mock
 import pytest
 import asyncio
+import time
 
 
 def test_basic_dspy_settings():
     dspy.configure(lm=dspy.LM("openai/gpt-4o"), adapter=dspy.JSONAdapter(), callbacks=[lambda x: x])
-    assert dspy.settings.lm.model == "gpt-4o"
+    assert dspy.settings.lm.model == "openai/gpt-4o"
     assert isinstance(dspy.settings.adapter, dspy.JSONAdapter)
     assert len(dspy.settings.callbacks) == 1
 
@@ -16,10 +17,10 @@ def test_basic_dspy_settings():
 def test_dspy_context():
     dspy.configure(lm=dspy.LM("openai/gpt-4o"), adapter=dspy.JSONAdapter(), callbacks=[lambda x: x])
     with dspy.context(lm=dspy.LM("openai/gpt-4o-mini"), callbacks=[]):
-        assert dspy.settings.lm.model == "gpt-4o-mini"
+        assert dspy.settings.lm.model == "openai/gpt-4o-mini"
         assert len(dspy.settings.callbacks) == 0
 
-    assert dspy.settings.lm.model == "gpt-4o"
+    assert dspy.settings.lm.model == "openai/gpt-4o"
     assert len(dspy.settings.callbacks) == 1
 
 
@@ -28,14 +29,14 @@ def test_dspy_context_parallel():
 
     def worker(i):
         with dspy.context(lm=dspy.LM("openai/gpt-4o-mini"), trace=[i], callbacks=[]):
-            assert dspy.settings.lm.model == "gpt-4o-mini"
+            assert dspy.settings.lm.model == "openai/gpt-4o-mini"
             assert dspy.settings.trace == [i]
             assert len(dspy.settings.callbacks) == 0
 
     with ThreadPoolExecutor(max_workers=5) as executor:
         executor.map(worker, range(3))
 
-    assert dspy.settings.lm.model == "gpt-4o"
+    assert dspy.settings.lm.model == "openai/gpt-4o"
     assert len(dspy.settings.callbacks) == 1
 
 
@@ -49,6 +50,7 @@ def test_dspy_context_with_dspy_parallel():
         def forward(self, question: str) -> str:
             lm = dspy.LM("openai/gpt-4o-mini", cache=False) if "France" in question else dspy.settings.lm
             with dspy.context(lm=lm):
+                time.sleep(1)
                 assert dspy.settings.lm.model == lm.model
                 return self.predict(question=question)
 
@@ -68,10 +70,13 @@ def test_dspy_context_with_dspy_parallel():
 
         # Verify mock was called correctly
         assert mock_completion.call_count == 2
-        # France question uses gpt-4o-mini
-        assert mock_completion.call_args_list[0].kwargs["model"] == "openai/gpt-4o-mini"
-        # Germany question uses gpt-4o
-        assert mock_completion.call_args_list[1].kwargs["model"] == "openai/gpt-4o"
+        for call_args in mock_completion.call_args_list:
+            if "France" in call_args.kwargs["messages"][-1]["content"]:
+                # France question uses gpt-4o-mini
+                assert call_args.kwargs["model"] == "openai/gpt-4o-mini"
+            else:
+                # Germany question uses gpt-4o
+                assert call_args.kwargs["model"] == "openai/gpt-4o"
 
         # The main thread is not affected by the context
         assert dspy.settings.lm.model == "openai/gpt-4o"
@@ -79,15 +84,20 @@ def test_dspy_context_with_dspy_parallel():
 
 @pytest.mark.asyncio
 async def test_dspy_context_with_async_task_group():
-    dspy.configure(lm=dspy.LM("openai/gpt-4o", cache=False), adapter=dspy.ChatAdapter())
+    dspy.configure(lm=dspy.LM("openai/gpt-4.1", cache=False), adapter=dspy.ChatAdapter())
 
     class MyModule(dspy.Module):
         def __init__(self):
             self.predict = dspy.Predict("question -> answer")
 
         async def aforward(self, question: str) -> str:
-            lm = dspy.LM("openai/gpt-4o-mini", cache=False) if "France" in question else dspy.settings.lm
+            lm = (
+                dspy.LM("openai/gpt-4o-mini", cache=False)
+                if "France" in question
+                else dspy.LM("openai/gpt-4o", cache=False)
+            )
             with dspy.context(lm=lm, trace=[]):
+                await asyncio.sleep(1)
                 assert dspy.settings.lm.model == lm.model
                 result = await self.predict.acall(question=question)
                 assert len(dspy.settings.trace) == 1
@@ -124,5 +134,5 @@ async def test_dspy_context_with_async_task_group():
         assert mock_completion.call_args_list[3].kwargs["model"] == "openai/gpt-4o"
 
         # The main thread is not affected by the context
-        assert dspy.settings.lm.model == "openai/gpt-4o"
+        assert dspy.settings.lm.model == "openai/gpt-4.1"
         assert dspy.settings.trace == []
