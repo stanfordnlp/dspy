63e11c3d35e819c029e9ba5b343a84ff3bc51187
TomuHirata <tomu.hirata@gmail.com>
2025-05-15 09:44:08 +0900

    fix

--- dspy/utils/__init__.py
+++ dspy/utils/__init__.py
@@ -1,13 +1,9 @@
-from dspy.utils.callback import BaseCallback, with_callbacks
-from dspy.utils.dummies import DummyLM, DummyVectorizer, dummy_rm
-from dspy.streaming.messages import StatusMessageProvider, StatusMessage
-from dspy.utils import exceptions
-
 import os
 
 import requests
 
 from dspy.streaming.messages import StatusMessage, StatusMessageProvider
+from dspy.utils import exceptions
 from dspy.utils.callback import BaseCallback, with_callbacks
 from dspy.utils.dummies import DummyLM, DummyVectorizer, dummy_rm
 
