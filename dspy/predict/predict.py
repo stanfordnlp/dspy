d3abb6324975d72aec65ad3e78e8d24bdfce9f95
chenmoneygithub <chen.qian@databricks.com>
2025-05-20 12:11:48 -0700

    support predicted output

--- dspy/predict/predict.py
+++ dspy/predict/predict.py
@@ -108,6 +108,17 @@ class Predict(Module, Parameter):
         if (temperature is None or temperature <= 0.15) and num_generations > 1:
             config["temperature"] = 0.7
 
+        if "prediction" in kwargs:
+            if (
+                isinstance(kwargs["prediction"], dict)
+                and kwargs["prediction"].get("type") == "content"
+                and "content" in kwargs["prediction"]
+            ):
+                # If the `prediction` is the standard predicted outputs format
+                # (https://platform.openai.com/docs/guides/predicted-outputs), we remvoe it from input kwargs and add it
+                # to the lm kwargs.
+                config["prediction"] = kwargs.pop("prediction")
+
         if not all(k in kwargs for k in signature.input_fields):
             present = [k for k in signature.input_fields if k in kwargs]
             missing = [k for k in signature.input_fields if k not in kwargs]
