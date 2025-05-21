a15c42cc6393a1b981252c65f62467d38b24b683
TomuHirata <tomu.hirata@gmail.com>
2025-05-21 13:54:56 +0900

    Run tests with extra dependencies separately (#8246)
    * run tests with extra dependencies separately
    
    * fix pipeline
    
    * add full_test dependency group
    
    * seperate test extras

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,7 +4,7 @@ import pytest
 
 from tests.test_utils.server import litellm_test_server, read_litellm_test_server_request_logs  # noqa: F401
 
-SKIP_DEFAULT_FLAGS = ["reliability"]
+SKIP_DEFAULT_FLAGS = ["reliability", "extra"]
 
 
 @pytest.fixture(autouse=True)
