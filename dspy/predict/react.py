4ea504138fe3cd8c04e57191a353f3b6ced64034
TomuHirata <tomu.hirata@gmail.com>
2025-05-30 05:36:59 +0900

    Add CodeAct module (#8222)
    * add CodeAct
    
    * fix type
    
    * change arg format
    
    * fix test
    
    * remove unused f string
    
    * address comments
    
    * address comments

--- dspy/predict/react.py
+++ dspy/predict/react.py
@@ -47,6 +47,7 @@ class ReAct(Module):
 
         for idx, tool in enumerate(tools.values()):
             instr.append(f"({idx + 1}) {tool}")
+        instr.append("When providing `next_tool_args`, the value inside the field must be in JSON format")
 
         react_signature = (
             dspy.Signature({**signature.input_fields}, "\n".join(instr))
