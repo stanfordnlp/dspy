3ded7a6266514c1569452b0e26bcd4e75c1ee6db
TomuHirata <tomu.hirata@gmail.com>
2025-06-16 01:02:05 -0700

    remove experimental module (#8391)

--- dspy/experimental/synthesizer/__init__.py
+++
@@ -1,7 +0,0 @@
-from dspy.experimental.synthesizer.config import SynthesizerArguments
-from dspy.experimental.synthesizer.synthesizer import Synthesizer
-
-__all__ = [
-    "Synthesizer",
-    "SynthesizerArguments",
-]
