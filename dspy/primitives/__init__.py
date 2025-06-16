53a31c52e990b97de250c16fa99d3e23e2416f46
TomuHirata <tomu.hirata@gmail.com>
2025-06-16 11:37:38 +0900

    remove commented out assertions file

--- dspy/primitives/__init__.py
+++ dspy/primitives/__init__.py
@@ -1,4 +1,3 @@
-from dspy.primitives import assertions
 from dspy.primitives.example import Example
 from dspy.primitives.module import BaseModule
 from dspy.primitives.prediction import Completions, Prediction
@@ -6,7 +5,6 @@ from dspy.primitives.program import Module, Program
 from dspy.primitives.python_interpreter import PythonInterpreter
 
 __all__ = [
-    "assertions",
     "Example",
     "BaseModule",
     "Prediction",
