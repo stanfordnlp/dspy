8b502564e12fe7bc0cccd7d9ab15b4cd07a34ef0
Ben McHone <ben@mchone.dev>
2025-08-11 21:11:10 -0500

    Ruff fix

--- dspy/adapters/__init__.py
+++ dspy/adapters/__init__.py
@@ -1,5 +1,5 @@
-from dspy.adapters.base import Adapter
 from dspy.adapters.baml_adapter import BAMLAdapter
+from dspy.adapters.base import Adapter
 from dspy.adapters.chat_adapter import ChatAdapter
 from dspy.adapters.json_adapter import JSONAdapter
 from dspy.adapters.two_step_adapter import TwoStepAdapter
