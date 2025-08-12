26248897242cfad6efb7119946613c71dc921240
Lakshya A Agrawal <lakshyaaagrawal@berkeley.edu>
2025-08-12 05:40:00 +0530

    Add GEPA (Genetic-Pareto) Optimizer (#8624)
    * Add GEPA Draft
    
    * Add GEPA paper to README
    
    * Improve GEPA Interface
    
    * Add Instruction Proposal function (commented)
    
    * Update imports
    
    * Fix README
    
    * Add track_best_outputs
    
    * Fix auto_budget bug
    
    * Fix bug in TraceData type
    
    * Fix ruff checks

--- dspy/teleprompt/__init__.py
+++ dspy/teleprompt/__init__.py
@@ -4,6 +4,7 @@ from dspy.teleprompt.bootstrap import BootstrapFewShot
 from dspy.teleprompt.bootstrap_finetune import BootstrapFinetune
 from dspy.teleprompt.copro_optimizer import COPRO
 from dspy.teleprompt.ensemble import Ensemble
+from dspy.teleprompt.gepa.gepa import GEPA
 from dspy.teleprompt.infer_rules import InferRules
 from dspy.teleprompt.knn_fewshot import KNNFewShot
 from dspy.teleprompt.mipro_optimizer_v2 import MIPROv2
@@ -20,6 +21,7 @@ __all__ = [
     "BootstrapFinetune",
     "COPRO",
     "Ensemble",
+    "GEPA",
     "KNNFewShot",
     "MIPROv2",
     "BootstrapFewShotWithRandomSearch",
