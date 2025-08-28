2b5e713c9deb0faebb3f09abe0c8be3b0c45439e
Crawford Collins <github@crawfordc.com>
2025-08-27 21:43:56 -0500

    add documentation and type hings

--- dspy/evaluate/evaluate.py
+++ dspy/evaluate/evaluate.py
@@ -29,6 +29,7 @@ except ImportError:
         """
         print(obj)
 
+
     def HTML(x: str) -> str:  # noqa: N802
         """
         Obtain the HTML representation of the specified string.
@@ -37,7 +38,6 @@ except ImportError:
         # environments where IPython is not available. In such environments where IPython is not
         # available, this method will simply return the input string.
         return x
-
 
 # TODO: Counting failures and having a max_failure count. When that is exceeded (also just at the end),
 # we print the number of failures, the first N examples that failed, and the first N exceptions raised.
@@ -55,7 +55,7 @@ class EvaluationResult(Prediction):
     """
 
     def __init__(
-        self, score: float, results: list[tuple["dspy.Example", "dspy.Example", Any]]
+            self, score: float, results: list[tuple["dspy.Example", "dspy.Example", Any]]
     ):
         super().__init__(score=score, results=results)
 
@@ -71,19 +71,19 @@ class Evaluate:
     """
 
     def __init__(
-        self,
-        *,
-        devset: list["dspy.Example"],
-        metric: Callable | None = None,
-        num_threads: int | None = None,
-        display_progress: bool = False,
-        display_table: bool | int = False,
-        max_errors: int | None = None,
-        provide_traceback: bool | None = None,
-        failure_score: float = 0.0,
-        save_as_csv=None,
-        save_as_json=None,
-        **kwargs,
+            self,
+            *,
+            devset: list["dspy.Example"],
+            metric: Callable | None = None,
+            num_threads: int | None = None,
+            display_progress: bool = False,
+            display_table: bool | int = False,
+            max_errors: int | None = None,
+            provide_traceback: bool | None = None,
+            failure_score: float = 0.0,
+            save_as_csv: str | None = None,
+            save_as_json: str | None = None,
+            **kwargs,
     ):
         """
         Args:
@@ -97,6 +97,9 @@ class Evaluate:
                 stopping evaluation. If ``None``, inherits from ``dspy.settings.max_errors``.
             provide_traceback (Optional[bool]): Whether to provide traceback information during evaluation.
             failure_score (float): The default score to use if evaluation fails due to an exception.
+            save_as_csv (Optional[str]): The file name where the csv will be saved.
+            save_as_json (Optional[str]): The file name where the json will be saved.
+
         """
         self.devset = devset
         self.metric = metric
@@ -116,16 +119,16 @@ class Evaluate:
 
     @with_callbacks
     def __call__(
-        self,
-        program: "dspy.Module",
-        metric: Callable | None = None,
-        devset: list["dspy.Example"] | None = None,
-        num_threads: int | None = None,
-        display_progress: bool | None = None,
-        display_table: bool | int | None = None,
-        callback_metadata: dict[str, Any] | None = None,
-        save_as_csv: str | None = None,
-        save_as_json: str | None = None,
+            self,
+            program: "dspy.Module",
+            metric: Callable | None = None,
+            devset: list["dspy.Example"] | None = None,
+            num_threads: int | None = None,
+            display_progress: bool | None = None,
+            display_table: bool | int | None = None,
+            callback_metadata: dict[str, Any] | None = None,
+            save_as_csv: str | None = None,
+            save_as_json: str | None = None,
     ) -> EvaluationResult:
         """
         Args:
@@ -240,8 +243,8 @@ class Evaluate:
             )
             data = self._prepare_results_output(results, metric_name)
             with open(
-                save_as_json,
-                "w",
+                    save_as_json,
+                    "w",
             ) as f:
                 json.dump(data, f)
 
@@ -252,7 +255,7 @@ class Evaluate:
 
     @staticmethod
     def _prepare_results_output(
-        results: list[tuple["dspy.Example", "dspy.Example", Any]], metric_name: str
+            results: list[tuple["dspy.Example", "dspy.Example", Any]], metric_name: str
     ):
         return [
             (
@@ -264,9 +267,9 @@ class Evaluate:
         ]
 
     def _construct_result_table(
-        self,
-        results: list[tuple["dspy.Example", "dspy.Example", Any]],
-        metric_name: str,
+            self,
+            results: list[tuple["dspy.Example", "dspy.Example", Any]],
+            metric_name: str,
     ) -> "pd.DataFrame":
         """
         Construct a pandas DataFrame from the specified result list.
@@ -294,7 +297,7 @@ class Evaluate:
         return result_df.rename(columns={"correct": metric_name})
 
     def _display_result_table(
-        self, result_df: "pd.DataFrame", display_table: bool | int, metric_name: str
+            self, result_df: "pd.DataFrame", display_table: bool | int, metric_name: str
     ):
         """
         Display the specified result DataFrame in a table format.
@@ -390,13 +393,13 @@ def display_dataframe(df: "pd.DataFrame"):
     else:
         # Pretty print the DataFrame to the console
         with pd.option_context(
-            "display.max_rows", None, "display.max_columns", None
+                "display.max_rows", None, "display.max_columns", None
         ):  # more options can be specified also
             print(df)
 
 
 def configure_dataframe_for_ipython_notebook_display(
-    df: "pd.DataFrame",
+        df: "pd.DataFrame",
 ) -> "pd.DataFrame":
     """Set various pandas display options for DataFrame in an IPython notebook environment."""
     import pandas as pd
@@ -418,7 +421,6 @@ def is_in_ipython_notebook_environment():
         return "IPKernelApp" in getattr(get_ipython(), "config", {})
     except ImportError:
         return False
-
 
 # FIXME: TODO: The merge_dicts stuff above is way too quick and dirty.
 # TODO: the display_table can't handle False but can handle 0!
