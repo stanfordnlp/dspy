da7853d5d67fb09b31b7a0d06f79501c14da3c91
TomuHirata <tomu.hirata@gmail.com>
2025-09-22 16:27:43 +0900

    Reimport EvaluationResult (#8843)

--- dspy/evaluate/__init__.py
+++ dspy/evaluate/__init__.py
@@ -1,5 +1,5 @@
 from dspy.evaluate.auto_evaluation import CompleteAndGrounded, SemanticF1
-from dspy.evaluate.evaluate import Evaluate
+from dspy.evaluate.evaluate import Evaluate, EvaluationResult
 from dspy.evaluate.metrics import EM, answer_exact_match, answer_passage_match, normalize_text
 
 __all__ = [
@@ -10,4 +10,5 @@ __all__ = [
     "Evaluate",
     "SemanticF1",
     "CompleteAndGrounded",
+    "EvaluationResult",
 ]
