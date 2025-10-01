616618eccf86700f5956395b4297310dc2d39c9d
chenmoneygithub <chen.qian@databricks.com>
2025-10-01 10:56:24 -0700

    fix condition check

--- dspy/streaming/streaming_listener.py
+++ dspy/streaming/streaming_listener.py
@@ -162,11 +162,13 @@ class StreamListener:
         if self.stream_start and chunk_message:
             # The stream is started, we keep returning the token until we see the start of the next field.
             token = None
+            self.field_end_queue.put(chunk_message)
             if self.field_end_queue.qsize() > 10:
                 # We keep the last 10 tokens in the buffer to check if they form a valid identifier for end_identifier,
                 # i.e., "[[ ## {next_field_name} ## ]]" for ChatAdapter to identify the end of the current field.
                 # In most cases 10 tokens are enough to cover the end_identifier for all adapters.
                 token = self.field_end_queue.get()
+
             concat_message = "".join(self.field_end_queue.queue).strip()
             if re.search(end_identifier, concat_message):
                 # The next field is identified, we can end the stream and flush out all tokens in the buffer.
