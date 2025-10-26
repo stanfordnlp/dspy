8ef090a6d5fb687194dd3ac57457d927c98b4e02
3dalgolab <threealgo@outlook.com>
2025-10-26 09:34:40 +0900

    Fix download function to handle zero remote size case

--- dspy/utils/__init__.py
+++ dspy/utils/__init__.py
@@ -16,7 +16,7 @@ def download(url):
     remote_size = int(requests.head(url, allow_redirects=True).headers.get("Content-Length", 0))
     local_size = os.path.getsize(filename) if os.path.exists(filename) else 0
 
-    if local_size != remote_size:
+    if local_size != remote_size or remote_size == 0:
         print(f"Downloading '{filename}'...")
         with requests.get(url, stream=True) as r, open(filename, "wb") as f:
             for chunk in r.iter_content(chunk_size=8192):
