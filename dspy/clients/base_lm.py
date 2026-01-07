18b2c1eaf899545d4dd2a3bc6806acad35524324
Marius Arvinte <marius.arvinte@intel.com>
2026-01-07 11:55:36 -0800

    One less change

--- dspy/clients/base_lm.py
+++ dspy/clients/base_lm.py
@@ -223,7 +223,6 @@ class BaseLM:
         if all(len(output) == 1 for output in outputs) or merged_kwargs.get("text_output", False):
             # Return a list if every output only has "text" key or if desired
             outputs = [output["text"] for output in outputs]
-
         return outputs
 
     def _extract_citations_from_response(self, choice):
