701ca7aeb302275ac1524be3e7cd921de6aacb99
at384 <at384@deutsche-boerse.com>
2026-02-24 11:53:02 +0100

    fix: run evaluation on main thread when num_threads=1 (#9150)

--- dspy/utils/parallelizer.py
+++ dspy/utils/parallelizer.py
@@ -47,6 +47,8 @@ class ParallelExecutor:
     def execute(self, function, data):
         tqdm.tqdm._instances.clear()
         wrapped = self._wrap_function(function)
+        if self.num_threads == 1:
+            return self._execute_sequential(wrapped, data)
         return self._execute_parallel(wrapped, data)
 
     def _wrap_function(self, user_function):
@@ -67,6 +69,57 @@ class ParallelExecutor:
                 return e
 
         return safe_func
+
+    def _execute_sequential(self, function, data):
+        """Execute items sequentially on the main thread.
+
+        This avoids spawning a ThreadPoolExecutor when num_threads == 1,
+        which is necessary for frameworks that require main-thread execution
+        (e.g. those using signal.signal()).
+        """
+        results = [None] * len(data)
+
+        pbar = tqdm.tqdm(
+            total=len(data),
+            dynamic_ncols=True,
+            disable=self.disable_progress_bar,
+            file=sys.stdout,
+        )
+
+        try:
+            for idx, item in enumerate(data):
+                if self.cancel_jobs.is_set():
+                    break
+
+                outcome = function(item)
+
+                if isinstance(outcome, Exception):
+                    self.failed_indices.append(idx)
+                    self.exceptions_map[idx] = outcome
+                else:
+                    results[idx] = outcome
+
+                if self.compare_results:
+                    vals = [r[-1] for r in results if r is not None]
+                    self._update_progress(pbar, sum(vals), len(vals))
+                else:
+                    self._update_progress(
+                        pbar,
+                        len([r for r in results if r is not None]),
+                        len(data),
+                    )
+        except KeyboardInterrupt:
+            self.cancel_jobs.set()
+            logger.warning("SIGINT received. Cancelling.")
+            raise
+        finally:
+            pbar.close()
+
+        if self.cancel_jobs.is_set():
+            logger.warning("Execution cancelled due to errors or interruption.")
+            raise Exception("Execution cancelled due to errors or interruption.")
+
+        return results
 
     def _execute_parallel(self, function, data):
         results = [None] * len(data)
